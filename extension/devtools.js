08a2a2cd92b100d2a46e7bc6bb821b5a6514fcb0
Jason <jasonbahl@mac.com>
2017-03-07 23:15:00 -0700

    Dev Tools Loading
    This adjusts the loading of the Dev Tools panel to load later if Apollo is added after the page load.

--- extension/devtools.js
+++ extension/devtools.js
@@ -1,10 +1,32 @@
-chrome.devtools.inspectedWindow.eval(
-  `!!(window.__APOLLO_CLIENT__)`,
-   function(result, isException) {
-     if (result) {
-       chrome.devtools.panels.create("Apollo", "imgs/logo.png", "dist/index.html", function(panel) {});
-     } else {
-       if (isException) console.warn(isException);
+var panelCreated = false;
+
+function createPanelIfApolloLoaded() {
+  if (panelCreated) {
+    return;
+  }
+  chrome.devtools.inspectedWindow.eval(
+    `!!(window.__APOLLO_CLIENT__)`,
+     function(result, isException) {
+       if (result) {
+         clearInterval(loadCheckInterval);
+         panelCreated = true;
+         chrome.devtools.panels.create("Apollo", "imgs/logo.png", "dist/index.html", function(panel) {});
+       } else {
+         if (isException) console.warn(isException);
+       }
      }
-   }
-);
+  );
+
+}
+
+chrome.devtools.network.onNavigated.addListener(function() {
+  createPanelIfApolloLoaded();
+});
+
+// Check to see if Apollo has loaded once per second in case Apollo is added
+// after page load
+var loadCheckInterval = setInterval(function() {
+  createPanelIfApolloLoaded();
+}, 1000);
+
+createPanelIfApolloLoaded();
