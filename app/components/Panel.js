94515c4405df6fbeb2efe0a569fecd81b300e09e
Ramya Nagarajan <ramyanaga.99@gmail.com>
2017-07-05 11:46:58 -0700

    Got store to show up again

--- app/components/Panel.js
+++ app/components/Panel.js
@@ -135,49 +135,49 @@ export default class Panel extends Component {
     //   });
      }, 100);
 
-    // this.initLogger();
+    this.initLogger();
   }
 
-  // initLogger() {
-  //   evalInPage(`
-  //     (function () {
-  //       let id = 1;
+  initLogger() {
+    evalInPage(`
+      (function () {
+        let id = 1;
 
-  //       if (window.__APOLLO_CLIENT__) {
-  //         window.__action_log__ = [];
+        if (window.__APOLLO_CLIENT__) {
+          window.__action_log__ = [];
 
-  //         const logger = (logItem) => {
-  //           // Only log Apollo actions for now
-  //           // type check 'type' to avoid issues with thunks and other middlewares
-  //           if (typeof logItem.action.type !== 'string' || logItem.action.type.split('_')[0] !== 'APOLLO') {
-  //             return;
-  //           }
+          const logger = (logItem) => {
+            // Only log Apollo actions for now
+            // type check 'type' to avoid issues with thunks and other middlewares
+            if (typeof logItem.action.type !== 'string' || logItem.action.type.split('_')[0] !== 'APOLLO') {
+              return;
+            }
 
-  //           id++;
+            id++;
 
-  //           logItem.id = id;
+            logItem.id = id;
 
-  //           window.__action_log__.push(logItem);
+            window.__action_log__.push(logItem);
 
-  //           if (window.__action_log__.length > 10) {
-  //             window.__action_log__.shift();
-  //           }
-  //         }
+            if (window.__action_log__.length > 10) {
+              window.__action_log__.shift();
+            }
+          }
 
-  //         window.__action_log__.push({
-  //           id: 0,
-  //           action: { type: 'INIT' },
-  //           state: window.__APOLLO_CLIENT__.queryManager.getApolloState(),
-  //           dataWithOptimisticResults: window.__APOLLO_CLIENT__.queryManager.getDataWithOptimisticResults(),
-  //         });
+          window.__action_log__.push({
+            id: 0,
+            action: { type: 'INIT' },
+            state: window.__APOLLO_CLIENT__.queryManager.getApolloState(),
+            dataWithOptimisticResults: window.__APOLLO_CLIENT__.queryManager.getDataWithOptimisticResults(),
+          });
 
-  //         //window.__APOLLO_CLIENT__.__actionHookForDevTools(logger);
-  //       }
-  //     })()
-  //   `, (result) => {
-  //     // Nothing
-  //   });
-  // }
+          //window.__APOLLO_CLIENT__.__actionHookForDevTools(logger);
+        }
+      })()
+    `, (result) => {
+      // Nothing
+    });
+  }
 
   componentWillUnmount() {
     clearInterval(this.interval);
