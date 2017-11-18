b6b7f45e7eb82bb65f1f429d94701df36833fc8c
Jeremy Gayed <tizmagik@users.noreply.github.com>
2017-11-17 22:49:36 -0500

    Update devtools.js
    Attempt to make Apollo panel creation more reliable

--- extension/devtools.js
+++ extension/devtools.js
@@ -1,15 +1,39 @@
-chrome.devtools.inspectedWindow.eval(`!!(window.__APOLLO_CLIENT__)`, function(
-  result,
-  isException
-) {
-  if (result) {
-    chrome.devtools.panels.create(
-      'Apollo',
-      './imgs/logo_devtools.png',
-      'dist/index.html',
-      function(panel) {}
-    );
-  } else {
-    if (isException) console.warn(isException);
+let panelCreated = false;
+
+function createPanel() {
+  if (panelCreated) {
+    return;
   }
-});
+
+  chrome.devtools.inspectedWindow.eval(
+    `!!(window.__APOLLO_CLIENT__);`,
+    function(result, isException) {
+      if (isException) {
+        console.warn(isException);
+      }
+
+      if (!result || panelCreated) {
+        return;
+      }
+
+      panelCreated = true;
+      clearInterval(loadCheckInterval);
+      chrome.devtools.panels.create(
+        'Apollo',
+        './imgs/logo_devtools.png',
+        'dist/index.html',
+        function(panel) {}
+      );
+    }
+  );
+}
+
+// Attempte to create Apollo panel on navigations as well
+chrome.devtools.network.onNavigated.addListener(createPanel);
+
+// Attempte to create Apollo panel once per second in case
+// Apollo is loaded after page load
+const loadCheckInterval = setInterval(createPanel, 1000);
+
+// Start the first attempt immediately
+createPanel();
