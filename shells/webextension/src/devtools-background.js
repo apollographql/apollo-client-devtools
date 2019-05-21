4ed94fb03062579472acced3f131eaed0747c64a
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2019-05-21 14:58:49 +0300

    Increased timeout period for checking whether to display panel

--- shells/webextension/src/devtools-background.js
+++ shells/webextension/src/devtools-background.js
@@ -30,7 +30,7 @@ function onPanelHidden() {
 
 function createPanel() {
   // stop trying if above 10 seconds or already made
-  if (panelCreated || checkCount++ > 10) return;
+  if (panelCreated || checkCount++ > 120) return;
 
   panelLoaded = false;
   panelShown = false;
