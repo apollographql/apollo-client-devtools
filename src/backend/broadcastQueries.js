9d225846bf97a31c4590467584a8e85a016ef802
hwillson <hugh@octonary.com>
2019-05-30 21:38:06 -0400

    Slight code/formatting tweaks

--- src/backend/broadcastQueries.js
+++ src/backend/broadcastQueries.js
@@ -1,23 +1,24 @@
 export const initBroadCastEvents = (hook, bridge) => {
-  //Counters for diagnostics
+  // Counters for diagnostics
   let counter = 0;
-  let sent = 0;
-  //Next broadcast to be sent
+
+  // Next broadcast to be sent
   let enqueued = null;
-  //Whether backend is ready for another broadcast
+
+  // Whether backend is ready for another broadcast
   let acknowledged = true;
-  //Threshold for warning about state size in Megabytes
+
+  // Threshold for warning about state size in Megabytes
   let warnMB = 10;
 
-  //Minimize impact to webpage. Serializing large state could cause Jank
+  // Minimize impact to webpage. Serializing large state could cause jank
   function scheduleBroadcast() {
     acknowledged = false;
     requestIdleCallback(sendBroadcast, { timeout: 120 /*max 2min*/ });
   }
 
-  //Send the Apollo broadcast to the devtools
+  // Send the Apollo broadcast to the devtools
   function sendBroadcast() {
-    sent++;
     const msg = JSON.stringify(enqueued);
     bridge.send("broadcast:new", msg);
     enqueued = null;
@@ -25,17 +26,16 @@ export const initBroadCastEvents = (hook, bridge) => {
     if (msg.length > warnMB * 1000000) {
       const currentMB = msg.length / 1000000;
       console.warn(
-        "Apollo DevTools serialized state is " +
-          currentMB.toFixed(1) +
-          "MB. This may cause performance degradation.",
+        `Apollo DevTools serialized state is ${currentMB.toFixed(1)} MB. ` +
+        "This may cause performance degradation.",
       );
-      warnMB = currentMB * 2; //Warn again if it doubles.
+      // Warn again if it doubles
+      warnMB = currentMB * 2;
     }
   }
 
   let logger = ({
     state: { queries, mutations },
-    // XXX replace with universal store format compat way of this
     dataWithOptimisticResults: inspector,
   }) => {
     counter++;
@@ -50,7 +50,7 @@ export const initBroadCastEvents = (hook, bridge) => {
     }
   };
 
-  //The backend has acknoledged reciept of a broadcast
+  // The backend has acknowledged receipt of a broadcast
   bridge.on("broadcast:ack", data => {
     acknowledged = true;
     if (enqueued) {
