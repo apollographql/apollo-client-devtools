110bcf4e9c7af5e00c36165232006691f4b6108c
Martha Schumann <mk.schumann@gmail.com>
2019-11-18 09:13:59 -0800

    adding devtools panel

--- static/index.js
+++ static/index.js
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom";
 import App from "../app/App";
 import "./index.css";
 
+chrome.devtools.panels.create(
+  'Apollo',
+  null, // TODO, path to icon
+  'index.html', // html page for injecting into the tab's content
+  null
+);
+
 ReactDOM.render(<App />, document.querySelector("#root"));
