07a1b7eb57a736659b686fc2e7032dd62ec6a935
Jenn Creighton <jenncreighton@jenns-mbp.home>
2020-07-07 15:39:25 -0400

    remove unneeded HttpLink

--- development/client/src/index.js
+++ development/client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider, makeReference } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, makeReference } from '@apollo/client';
 
 const client = new ApolloClient({
   cache: new InMemoryCache({
@@ -25,9 +25,7 @@ const client = new ApolloClient({
       }
     },
   }),
-  link: new HttpLink({
-    uri: 'http://localhost:4000',
-  }),
+  uri: 'http://localhost:4000',
 });
 
 ReactDOM.render(
