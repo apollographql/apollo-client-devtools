4d56f7e64458c37274b69f658532d6dcd0d5f38c
Jenn Creighton <jenncreighton@jenns-mbp.home>
2020-08-11 17:51:46 -0400

    Modify Relay tests to be async

--- src/__tests__/Relay.test.ts
+++ src/__tests__/Relay.test.ts
@@ -1,86 +1,95 @@
 import Relay from '../Relay';
 
 describe('Relay', () => {
-  it('sends a message to a connection', () => {
+  it('sends a message to a connection', () => new Promise(resolve => {
     const relay = new Relay();
-    const callback = jest.fn();
-    relay.addConnection('Bedford Ave', callback);
+    relay.addConnection('Bedford Ave', resolve);
 
     relay.send({
       message: 'The L train is delayed.',
       to: 'Bedford Ave',
     });
-
-    expect(callback).toBeCalledWith({ 
-      message: 'The L train is delayed.', 
+  }).then(result => {
+    expect(result).toEqual({
+      message: 'The L train is delayed.',
       to: undefined,
-    });
-  });
+     });
+  }));
 
-  it('sends the next destination to the connection', () => {
+  it('sends the next destination to the connection', () => new Promise(resolve => {
     const relay = new Relay();
-    const callback = jest.fn();
-    relay.addConnection('Bedford Ave', callback);
+    relay.addConnection('Bedford Ave', resolve);
 
     relay.send({
       message: 'The L train is delayed.',
       to: 'Bedford Ave:Lorimer St',
     });
+  }).then(result => {
+    expect(result).toEqual({
+      message: 'The L train is delayed.',
+      to: 'Lorimer St',
+     });
+  }));
 
-    expect(callback).toBeCalledWith({ 
-      message: 'The L train is delayed.', 
-      to: 'Lorimer St'
-    });
-  });
-
-  it('can remove a connection', () => {
+  it('can remove a connection', () => new Promise((resolve, reject) => {
     const relay = new Relay();
-    const callback = jest.fn();
+    const callback = jest.fn(result => {
+      try {
+        expect(callback).toHaveBeenCalledTimes(1);
+        expect(result).toEqual({
+          message: 'The L train is delayed.',
+          to: undefined,
+        });
+
+        relay.removeConnection('Bedford Ave');
+        // A second message will cause a test failure if the connection has not been removed.
+        relay.send({
+          message: 'The L train is delayed.',
+          to: 'Bedford Ave',
+        });
+
+        // We set a short delay to allow the above to fail.
+        setTimeout(resolve, 100);
+      } catch (e) {
+        reject(e);
+      }
+    });
+
     relay.addConnection('Bedford Ave', callback);
 
     relay.send({
       message: 'The L train is delayed.',
       to: 'Bedford Ave',
     });
+  }));
 
-    expect(callback).toBeCalledWith({ 
-      message: 'The L train is delayed.', 
-      to: undefined,
-    });
-
-    relay.removeConnection('Bedford Ave');
-
-    relay.send({
-      message: 'The L train is still delayed.',
-      to: 'Bedford Ave',
-    });
-
-    expect(callback).not.toBeCalledTimes(2);
-  });
-
-  it('can listen for a message', () => {
+  it('can listen for a message', () => new Promise(resolve => {
     const relay = new Relay();
-    const callback = jest.fn();
-
-    relay.listen('Train delayed.', callback);
+    relay.listen('Train delayed.', resolve);
     relay.broadcast({ message: 'Train delayed.' });
+  }).then(result => {
+    const customEvent = new CustomEvent('Train delayed.', { detail: {} });
+    expect(result).toEqual(customEvent);
+  }));
 
-    expect(callback).toBeCalled();
-  });
-
-  it('returns a removeEventListener function', () => {
+  it('returns a removeEventListener function', () => new Promise((resolve, reject) => {
     const relay = new Relay();
-    const callback = jest.fn();
+    const callback = jest.fn(result => {
+      try {
+        expect(callback).toBeCalledTimes(1);
+        expect(result).toEqual(new CustomEvent('Train delayed.', { detail: {} }));
+        removeEventListener();
+        // A second broadcast will cause a test failure if the listener has not been removed.
+        relay.broadcast({ message: 'Train delayed.' });
+
+        // We set a short delay to allow the above to fail.
+        setTimeout(resolve, 1000);
+      } catch(e) {
+        reject(e);
+      }
+    });
 
     const removeEventListener = relay.listen('Train delayed.', callback);
     relay.broadcast({ message: 'Train delayed.' });
-
-    expect(callback).toBeCalled();
-
-    removeEventListener();
-
-    relay.broadcast({ message: 'Train delayed.' });
-
-    expect(callback).not.toBeCalledTimes(2);
-  });
+  }));
 });
