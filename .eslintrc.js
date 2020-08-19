34434c3bc9c59f3b0412ae11662e487f521c29d1
Jenn Creighton <jenncreighton@jenns-mbp.home>
2020-08-19 17:30:35 -0400

    Enable ESLint with @typescript-eslint and React-specific rules

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  env: {
+    'browser': true,
+    'node': true,
+    'webextensions': true,
+  },
+  rules: {
+    'react/prop-types': 0,
+  }
+};
