99a0f26567821ec0f661d7402cf32b617a339d4a
Jenn Creighton <jenncreighton@jenns-mbp.home>
2020-09-17 16:28:58 -0400

    Integrate GraphiQL Explorer

--- src/application/Explorer/Explorer.tsx
+++ src/application/Explorer/Explorer.tsx
@@ -1,16 +1,14 @@
 import React, { useRef, useState, useEffect } from "react";
 import GraphiQL from "graphiql";
 import { Observable , useQuery, gql } from "@apollo/client";
+import { getIntrospectionQuery, buildClientSchema } from "graphql/utilities";
 import { parse } from "graphql/language/parser";
 import { print } from "graphql/language/printer";
+import GraphiQLExplorer from "graphiql-explorer";
 import { graphiQLQuery, ColorThemes } from '../index';
 import { sendGraphiQLRequest, receiveGraphiQLResponses, listenForResponse } from './graphiQLRelay';
 
 import "../../../node_modules/graphiql/graphiql.css";
-
-// TODO: Run IntrospectionQuery outside GraphiQL component 
-// TODO: Integrate GraphiQL Explorer
-// TODO: "Run in GraphiQL"
 
 const GET_EXPLORER_DATA = gql`
   query GetExplorerData {
@@ -27,13 +25,43 @@ enum FetchPolicies {
 type FetchPolicy = FetchPolicies;
 
 export const Explorer = () => {
-  const graphiQLRef = useRef<any>(null);
+  const graphiQLRef = useRef<any>();
+  const [schema, setSchema] = useState();
+  const [isExplorerOpen, setIsExplorerOpen] = useState<boolean>(false);
   const [queryCache, setQueryCache] = useState<FetchPolicy>(FetchPolicies.NoCache);
 
   const { data, loading, error } = useQuery(GET_EXPLORER_DATA);
+
+  const executeOperation = ({ query, operationName, variables, fetchPolicy = queryCache }) => new Observable(observer => {
+    const payload = JSON.stringify({
+      query,
+      operationName,
+      variables,
+      fetchPolicy,
+    });
+    sendGraphiQLRequest(payload);
+    listenForResponse(operationName, payload => {
+      observer.next(payload);
+      observer.complete();
+    });
+  }) as any;
+
   // Subscribe to GraphiQL data responses
   // Returns a cleanup method to useEffect
   useEffect(() => receiveGraphiQLResponses());
+
+  useEffect(() => {
+    const observer = executeOperation({ 
+      query: getIntrospectionQuery(), 
+      operationName: 'IntrospectionQuery', 
+      variables: null,
+      fetchPolicy: FetchPolicies.NoCache, 
+    });
+
+    observer.subscribe(response => {
+      setSchema(buildClientSchema(response.data) as any);
+    });
+  }, []);
 
   const handleClickPrettifyButton = () => {
     const editor = graphiQLRef.current?.getQueryEditor();
@@ -42,6 +70,8 @@ export const Explorer = () => {
     editor.setValue(prettyText);
   };
 
+  const handleToggleExplorer = () => setIsExplorerOpen(!isExplorerOpen);
+
   if (loading || !data || error) {
     // TODO: Proper loading / error states
     return null;
@@ -49,32 +79,31 @@ export const Explorer = () => {
 
   return (
     <div className="graphiql-container">
+      <GraphiQLExplorer
+        schema={schema}
+        query={data.graphiQLQuery}
+        onEdit={query => graphiQLQuery(query)}
+        explorerIsOpen={isExplorerOpen}
+        onToggleExplorer={handleToggleExplorer}
+      />
       <GraphiQL
         ref={graphiQLRef}
-        fetcher={({ query, operationName, variables }) => new Observable(observer => {
-          const payload = JSON.stringify({
-            query,
-            operationName,
-            variables,
-            fetchPolicy: operationName === 'IntrospectionQuery' ? FetchPolicies.NoCache : queryCache
-          });
-          sendGraphiQLRequest(payload);
-          listenForResponse(operationName, payload => {
-            observer.next(payload);
-            observer.complete();
-          });
-        }) as any}
+        fetcher={executeOperation}
+        schema={schema}
         query={data.graphiQLQuery}
         editorTheme={data.colorTheme === ColorThemes.Dark ? 'dracula' : 'graphiql'}
-        onEditQuery={query => {
-          graphiQLQuery(query);
-        }}
+        onEditQuery={query => graphiQLQuery(query)}
       >
           <GraphiQL.Toolbar>
             <GraphiQL.Button
               label="Prettify"
               title="Prettify"
               onClick={handleClickPrettifyButton}
+            />
+            <GraphiQL.Button
+              label="Explorer"
+              title="Toggle Explorer"
+              onClick={handleToggleExplorer}
             />
             <label>
               <input
