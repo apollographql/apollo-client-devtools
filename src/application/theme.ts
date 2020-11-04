25ae17e0c2b4698f179efcdc632ab9341a05e64f
Jenn Creighton <jenncreighton@jenns-mbp.home>
2020-11-04 13:44:06 -0500

    Adjust types

--- src/application/theme.ts
+++ src/application/theme.ts
@@ -1,5 +1,5 @@
 import { lighten } from "polished";
-import { colors } from "@apollo/space-kit/colors";
+import { colors, ShadedColor } from "@apollo/space-kit/colors";
 
 export enum ColorTheme {
   Light = 'light',
@@ -10,15 +10,17 @@ const shared = {
   sidebarSelected: colors.indigo.base,
 };
 
-export const themes: Record<ColorTheme, Record<string, string>> = {
+export const themes: Record<ColorTheme, Record<string, ShadedColor>> = {
   [ColorTheme.Light]: {
     primary: colors.indigo.darkest,
-    sidebarHover: lighten(0.2, colors.indigo.darkest),
+    sidebarHover: lighten(0.2, colors.indigo.darkest) as ShadedColor,
     ...shared,
   },
   [ColorTheme.Dark]: {
     primary: colors.black.base,
-    sidebarHover: lighten(0.2, colors.black.base),
+    sidebarHover: lighten(0.2, colors.black.base) as ShadedColor,
     ...shared,
   },
 };
+
+export type Theme = (typeof themes)[keyof typeof themes];
