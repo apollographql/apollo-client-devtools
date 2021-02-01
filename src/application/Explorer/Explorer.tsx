5d2dc664665c3c0c0ffe7abac0b42e301f7f71ef
hwillson <hugh@octonary.com>
2021-02-01 14:51:32 -0500

    Replace displayed "Explorer" labels with "Build"
    To help reduce confusion between `graphiql-explorer` and Studio's
    Explorer.

--- src/application/Explorer/Explorer.tsx
+++ src/application/Explorer/Explorer.tsx
@@ -5,29 +5,38 @@ import { colors } from "@apollo/space-kit/colors";
 import { useRef, useState, useEffect, Fragment } from "react";
 // @ts-ignore
 import GraphiQL from "@forked/graphiql";
-import { Observable, makeVar, useReactiveVar, FetchResult } from "@apollo/client";
+import {
+  Observable,
+  makeVar,
+  useReactiveVar,
+  FetchResult,
+} from "@apollo/client";
 import type { GraphQLSchema, IntrospectionQuery } from "graphql";
 import { getIntrospectionQuery, buildClientSchema } from "graphql/utilities";
 import { parse } from "graphql/language/parser";
 import { print } from "graphql/language/printer";
 import GraphiQLExplorer from "graphiql-explorer";
 import { Button } from "@apollo/space-kit/Button";
-import { useTheme, ColorTheme, colorTheme } from '../theme';
-import { sendGraphiQLRequest, receiveGraphiQLResponses, listenForResponse } from './graphiQLRelay';
-import { FullWidthLayout } from '../Layouts/FullWidthLayout';
+import { useTheme, ColorTheme, colorTheme } from "../theme";
+import {
+  sendGraphiQLRequest,
+  receiveGraphiQLResponses,
+  listenForResponse,
+} from "./graphiQLRelay";
+import { FullWidthLayout } from "../Layouts/FullWidthLayout";
 
 import "@forked/graphiql-css";
 
 enum FetchPolicy {
-  NoCache = 'no-cache',
-  CacheOnly = 'cache-only'
+  NoCache = "no-cache",
+  CacheOnly = "cache-only",
 }
 
-export const graphiQLQuery = makeVar<string>('');
+export const graphiQLQuery = makeVar<string>("");
 export const graphiQLSchema = makeVar<GraphQLSchema | undefined>(undefined);
 
 export const resetGraphiQLVars = () => {
-  graphiQLQuery('');
+  graphiQLQuery("");
   graphiQLSchema(undefined);
 };
 
@@ -42,6 +51,14 @@ const headerStyles = css`
 
 const mainStyles = css`
   display: flex;
+
+  .docExplorerWrap > .doc-explorer-title-bar > .doc-explorer-title {
+    font-size: 0;
+    &:after {
+      font-size: ${rem(16)};
+      content: "Build";
+    }
+  }
 `;
 
 const buttonContainerStyles = css`
@@ -118,37 +135,50 @@ const borderStyles = css`
   border-right: ${rem(1)} var(--whiteTransparent);
 `;
 
+const explorerStyles = css`
+  .doc-explorer-title {
+    font-size: 0;
+    &:after {
+      font-size: ${rem(16)};
+      content: "Build";
+    }
+  }
+`;
+
 export const Explorer = ({ navigationProps }) => {
   const graphiQLRef = useRef<GraphiQL>(null);
   const schema = useReactiveVar(graphiQLSchema);
   const [isExplorerOpen, setIsExplorerOpen] = useState<boolean>(false);
   const [isDocsOpen, setIsDocsOpen] = useState<boolean>(false);
-  const [queryCache, setQueryCache] = useState<FetchPolicy>(FetchPolicy.NoCache);
+  const [queryCache, setQueryCache] = useState<FetchPolicy>(
+    FetchPolicy.NoCache
+  );
 
   const color = useReactiveVar(colorTheme);
   const query = useReactiveVar(graphiQLQuery);
 
   const theme = useTheme();
 
-  const executeOperation = ({ 
-    query, 
-    operationName, 
-    variables, 
+  const executeOperation = ({
+    query,
+    operationName,
+    variables,
     fetchPolicy = queryCache,
-  }) => new Observable<FetchResult>(observer => {
-    const payload = JSON.stringify({
-      query,
-      operationName,
-      variables,
-      fetchPolicy,
-    });
+  }) =>
+    new Observable<FetchResult>((observer) => {
+      const payload = JSON.stringify({
+        query,
+        operationName,
+        variables,
+        fetchPolicy,
+      });
 
-    sendGraphiQLRequest(payload);
-    listenForResponse(operationName, payload => {
-      observer.next(payload);
-      observer.complete();
+      sendGraphiQLRequest(payload);
+      listenForResponse(operationName, (payload) => {
+        observer.next(payload);
+        observer.complete();
+      });
     });
-  });
 
   // Subscribe to GraphiQL data responses
   // Returns a cleanup method to useEffect
@@ -156,14 +186,14 @@ export const Explorer = ({ navigationProps }) => {
 
   useEffect(() => {
     if (!schema) {
-      const observer = executeOperation({ 
-        query: getIntrospectionQuery(), 
-        operationName: 'IntrospectionQuery', 
+      const observer = executeOperation({
+        query: getIntrospectionQuery(),
+        operationName: "IntrospectionQuery",
         variables: null,
-        fetchPolicy: FetchPolicy.NoCache, 
+        fetchPolicy: FetchPolicy.NoCache,
       });
-  
-      observer.subscribe(response => {
+
+      observer.subscribe((response) => {
         graphiQLSchema(buildClientSchema(response.data as IntrospectionQuery));
       });
     }
@@ -177,95 +207,95 @@ export const Explorer = ({ navigationProps }) => {
   };
 
   const handleToggleExplorer = () => setIsExplorerOpen(!isExplorerOpen);
-  const handleToggleDocs = () => setIsDocsOpen(!isDocsOpen);;
+  const handleToggleDocs = () => setIsDocsOpen(!isDocsOpen);
 
   return (
-    <FullWidthLayout 
-      navigationProps={navigationProps}
-    >
-
-        <GraphiQL
-          ref={graphiQLRef}
-          fetcher={(args => {
+    <FullWidthLayout navigationProps={navigationProps}>
+      <GraphiQL
+        ref={graphiQLRef}
+        fetcher={
+          ((args) => {
             // Ignore IntrospectionQuery from GraphiQL to prevent redundant introspections
             // We duplicate this call above so GraphiQLExplorer can access the schema
-            if (args.operationName === 'IntrospectionQuery') {
+            if (args.operationName === "IntrospectionQuery") {
               return Promise.resolve({});
             }
 
             return executeOperation(args);
-          }) as any}
-          schema={schema}
-          query={query}
-          editorTheme={color === ColorTheme.Dark ? 'dracula' : 'graphiql'}
-          onEditQuery={newQuery => graphiQLQuery(newQuery)}
-          render={({ ExecuteButton, GraphiQLEditor, DocExplorer }) => {
-            return (
-              <Fragment>   
-                <FullWidthLayout.Header 
-                  css={headerStyles}
-                >
-                  <div css={borderStyles}></div>
-                  <div css={buttonContainerStyles}>
-                    <ExecuteButton css={[buttonStyles, runButtonStyles]} />
-                    <Button
-                      css={buttonStyles}
-                      title="Prettify"
-                      feel="flat"
-                      onClick={handleClickPrettifyButton}
-                    >
-                      Prettify
-                    </Button>
-                    <Button
-                      css={buttonStyles}
-                      title="Toggle Explorer"
-                      feel="flat"
-                      onClick={handleToggleExplorer}
-                    >
-                      Explorer
-                    </Button>
-                  </div>
-                  <div css={borderStyles}></div>
-                  <label 
-                    htmlFor="loadFromCache"
-                    css={labelStyles}
-                  >
-                    <input
-                      id="loadFromCache"
-                      css={checkboxStyles}
-                      type="checkbox"
-                      name="loadFromCache"
-                      checked={queryCache === FetchPolicy.CacheOnly}
-                      onChange={() => setQueryCache(queryCache === FetchPolicy.CacheOnly ? FetchPolicy.NoCache : FetchPolicy.CacheOnly)}
-                    />
-                    Load from cache
-                  </label>
+          }) as any
+        }
+        schema={schema}
+        query={query}
+        editorTheme={color === ColorTheme.Dark ? "dracula" : "graphiql"}
+        onEditQuery={(newQuery) => graphiQLQuery(newQuery)}
+        render={({ ExecuteButton, GraphiQLEditor, DocExplorer }) => {
+          return (
+            <Fragment>
+              <FullWidthLayout.Header css={headerStyles}>
+                <div css={borderStyles}></div>
+                <div css={buttonContainerStyles}>
+                  <ExecuteButton css={[buttonStyles, runButtonStyles]} />
                   <Button
-                    css={docsButtonStyles}
-                    title="Open Document Explorer"
+                    css={buttonStyles}
+                    title="Prettify"
                     feel="flat"
-                    onClick={handleToggleDocs}
+                    onClick={handleClickPrettifyButton}
                   >
-                    Docs
+                    Prettify
                   </Button>
-                </FullWidthLayout.Header>
-                <FullWidthLayout.Main
-                  css={mainStyles}
+                  <Button
+                    css={buttonStyles}
+                    title="Toggle Explorer"
+                    feel="flat"
+                    onClick={handleToggleExplorer}
+                  >
+                    Build
+                  </Button>
+                </div>
+                <div css={borderStyles}></div>
+                <label htmlFor="loadFromCache" css={labelStyles}>
+                  <input
+                    id="loadFromCache"
+                    css={checkboxStyles}
+                    type="checkbox"
+                    name="loadFromCache"
+                    checked={queryCache === FetchPolicy.CacheOnly}
+                    onChange={() =>
+                      setQueryCache(
+                        queryCache === FetchPolicy.CacheOnly
+                          ? FetchPolicy.NoCache
+                          : FetchPolicy.CacheOnly
+                      )
+                    }
+                  />
+                  Load from cache
+                </label>
+                <Button
+                  css={docsButtonStyles}
+                  title="Open Document Explorer"
+                  feel="flat"
+                  onClick={handleToggleDocs}
                 >
+                  Docs
+                </Button>
+              </FullWidthLayout.Header>
+              <FullWidthLayout.Main css={mainStyles}>
+                <div css={explorerStyles}>
                   <GraphiQLExplorer
                     schema={schema}
                     query={query}
-                    onEdit={newQuery => graphiQLQuery(newQuery)}
+                    onEdit={(newQuery) => graphiQLQuery(newQuery)}
                     explorerIsOpen={isExplorerOpen}
                     onToggleExplorer={handleToggleExplorer}
                   />
-                  {GraphiQLEditor}
-                  {isDocsOpen && <DocExplorer onToggleDocs={handleToggleDocs} />}
-                </FullWidthLayout.Main>
-              </Fragment>
-            );
-          }}
-        />
+                </div>
+                {GraphiQLEditor}
+                {isDocsOpen && <DocExplorer onToggleDocs={handleToggleDocs} />}
+              </FullWidthLayout.Main>
+            </Fragment>
+          );
+        }}
+      />
     </FullWidthLayout>
   );
 };
