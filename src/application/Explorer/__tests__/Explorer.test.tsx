c71eb84c62f768a4b85f7105c583582112b63043
hwillson <hugh@octonary.com>
2021-02-05 15:34:17 -0500

    Test updates

--- src/application/Explorer/__tests__/Explorer.test.tsx
+++ src/application/Explorer/__tests__/Explorer.test.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import { within, waitForElementToBeRemoved, screen } from '@testing-library/react';
-import user from '@testing-library/user-event';
-import { renderWithApolloClient } from '../../utilities/testing/renderWithApolloClient';
-import { Explorer } from '../Explorer';
-import { listenForResponse } from '../graphiQLRelay';
+import React from "react";
+import {
+  within,
+  waitForElementToBeRemoved,
+  screen,
+} from "@testing-library/react";
+import user from "@testing-library/user-event";
+import { renderWithApolloClient } from "../../utilities/testing/renderWithApolloClient";
+import { Explorer } from "../Explorer";
+import { listenForResponse } from "../graphiQLRelay";
 
-import { graphql } from 'graphql';
+import { graphql } from "graphql";
 import { getIntrospectionQuery } from "graphql/utilities";
-import { schemaWithMocks } from '../../utilities/testing/fakeGraphQL';
+import { schemaWithMocks } from "../../utilities/testing/fakeGraphQL";
 
 // Required to prevent an error in CodeMirror
 document.createRange = () => {
@@ -19,67 +23,75 @@ document.createRange = () => {
     return {
       item: () => null,
       length: 0,
-      [Symbol.iterator]: jest.fn()
+      [Symbol.iterator]: jest.fn(),
     };
   };
 
   return range;
-}
+};
 
-jest.mock('../graphiQLRelay', () => ({
-  sendGraphiQLRequest: jest.fn(), 
-  receiveGraphiQLResponses: jest.fn(), 
+jest.mock("../graphiQLRelay", () => ({
+  sendGraphiQLRequest: jest.fn(),
+  receiveGraphiQLResponses: jest.fn(),
   listenForResponse: jest.fn(),
 }));
 
-describe('<Explorer />', () => {
+describe("<Explorer />", () => {
   const navigationProps = { queriesCount: 0, mutationsCount: 0 };
-  test('it renders a header', () => {
-    const { getByTestId } = renderWithApolloClient(<Explorer navigationProps={navigationProps} />);
-    const header = getByTestId('header');
+  test("it renders a header", () => {
+    const { getByTestId } = renderWithApolloClient(
+      <Explorer navigationProps={navigationProps} />
+    );
+    const header = getByTestId("header");
     const { getByText, getByLabelText } = within(header);
-  
+
     expect(header).toBeInTheDocument();
-    expect(getByText('Prettify')).toBeInTheDocument();
-    expect(getByText('Explorer')).toBeInTheDocument();
-    expect(getByLabelText('Load from cache')).toBeInTheDocument();
-    expect(getByText('Docs')).toBeInTheDocument();
+    expect(getByText("Prettify")).toBeInTheDocument();
+    expect(getByText("Build")).toBeInTheDocument();
+    expect(getByLabelText("Load from cache")).toBeInTheDocument();
+    expect(getByText("Docs")).toBeInTheDocument();
   });
 
-  test('it can toggle the GraphiQLExplorer component', () => {
-    const { container, getByTestId } = renderWithApolloClient(<Explorer navigationProps={navigationProps} />);
-    const header = getByTestId('header');
+  test("it can toggle the GraphiQLExplorer component", () => {
+    const { container, getByTestId } = renderWithApolloClient(
+      <Explorer navigationProps={navigationProps} />
+    );
+    const header = getByTestId("header");
     const { getByText } = within(header);
 
-    const button = getByText('Explorer');
-    const explorer = container.querySelector('.docExplorerWrap');
+    const button = getByText("Build");
+    const explorer = container.querySelector(".docExplorerWrap");
     expect(explorer).not.toBeVisible();
     user.click(button as HTMLElement);
     expect(explorer).toBeVisible();
   });
 
-  test('it can toggle the DocExplorer component', () => {
-    const { getByTestId } = renderWithApolloClient(<Explorer navigationProps={navigationProps} />);
-    const header = getByTestId('header');
+  test("it can toggle the DocExplorer component", () => {
+    const { getByTestId } = renderWithApolloClient(
+      <Explorer navigationProps={navigationProps} />
+    );
+    const header = getByTestId("header");
     const { getByText: getByTextInHeader } = within(header);
-    const button = getByTextInHeader('Docs');
+    const button = getByTextInHeader("Docs");
 
-    expect(screen.queryByText('DocExplorer')).not.toBeInTheDocument;
+    expect(screen.queryByText("DocExplorer")).not.toBeInTheDocument;
     user.click(button as HTMLElement);
-    expect(screen.queryByText('DocExplorer')).toBeInTheDocument;
+    expect(screen.queryByText("DocExplorer")).toBeInTheDocument;
   });
 
-  test('it retrieves a schema from an IntrospectionQuery', async () => {
-    (listenForResponse as any).mockImplementation(((_, cb) => {
-      graphql(schemaWithMocks, getIntrospectionQuery()).then((result) => cb(result));
-    }), );
+  test("it retrieves a schema from an IntrospectionQuery", async () => {
+    (listenForResponse as any).mockImplementation((_, cb) => {
+      graphql(schemaWithMocks, getIntrospectionQuery()).then((result) =>
+        cb(result)
+      );
+    });
 
-    const { getByText } = renderWithApolloClient(<Explorer navigationProps={navigationProps} />);
-    const NoSchemaError = getByText('No Schema Available');
+    const { getByText } = renderWithApolloClient(
+      <Explorer navigationProps={navigationProps} />
+    );
+    const NoSchemaError = getByText("No Schema Available");
     expect(NoSchemaError).toBeInTheDocument();
 
     waitForElementToBeRemoved(NoSchemaError);
   });
 });
-
-
