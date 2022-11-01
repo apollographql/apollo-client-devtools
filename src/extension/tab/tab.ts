7103c06bea810b49ed6f6b08628fc4a3fc697b10
Zhao <hronro@gmail.com>
2022-11-02 02:44:27 +0800

    fix: do not break Import Maps (#734)
    * fix: do not break Import Maps
    
    * Add to importmap after DOMContentLoaded
    
    Co-authored-by: Emmanuel S <emmanuelssr@gmail.com>

--- src/extension/tab/tab.ts
+++ src/extension/tab/tab.ts
@@ -10,6 +10,13 @@ if (typeof document === "object" && document instanceof HTMLDocument) {
   const script = document.createElement("script");
   script.setAttribute("type", "module");
   script.setAttribute("src", chrome.extension.getURL("hook.js"));
-  const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
-  head.insertBefore(script, head.lastChild);
+  document.addEventListener("DOMContentLoaded", () => {
+    const importMap = document.querySelector("script[type=\"importmap\"]");
+    if (importMap != null) {
+      importMap.parentNode?.insertBefore(script, importMap.nextSibling);
+    } else {
+      const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+      head.insertBefore(script, head.lastChild);
+    }
+  });
 }
