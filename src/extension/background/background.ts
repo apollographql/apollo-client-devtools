6739d5623afa7eab16baa59ddbf59ca142876d45
Jerel Miller <jerelmiller@gmail.com>
2023-03-06 10:57:16 -0700

    Use `browser` namespace with `webextension-polyfill` instead of `chrome` namespace (#993)

--- src/extension/background/background.ts
+++ src/extension/background/background.ts
@@ -2,17 +2,18 @@ import Relay from '../../Relay';
 import { 
   REQUEST_TAB_ID, 
 } from '../constants';
+import browser from 'webextension-polyfill';
 
 // This sends the tab id to the inspected tab.
-chrome.runtime.onMessage.addListener(({ message }, sender, sendResponse) => {
+browser.runtime.onMessage.addListener(({ message }, sender) => {
   if (message === REQUEST_TAB_ID) {
-    sendResponse(sender?.tab?.id);
+    return Promise.resolve(sender.tab?.id);
   }
 });
 
 const background = new Relay();
 
-chrome.runtime.onConnect.addListener(port => {
+browser.runtime.onConnect.addListener(port => {
   background.addConnection(port.name, message => {
     port.postMessage(message);
   });
