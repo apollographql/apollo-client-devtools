fc964ed06aa36ea568d398c85cb27c35a282f6bf
Jerel Miller <jerelmiller@gmail.com>
2023-03-06 09:51:27 -0700

    Use early return to unnest some code

--- src/extension/devtools/devtools.ts
+++ src/extension/devtools/devtools.ts
@@ -50,92 +50,94 @@ devtools.addConnection(EXPLORER_SUBSCRIPTION_TERMINATION, () => {
 });
 
 devtools.listen(CREATE_DEVTOOLS_PANEL, async ({ payload }) => {
-  if (!isPanelCreated) {
-    const panel = await browser.devtools.panels.create(
-      "Apollo",
-      "logo_devtools.png",
-      "panel.html",
-    );
+  if (isPanelCreated) {
+    return;
+  }
 
-    isPanelCreated = true;
-    const { queries, mutations, cache } = JSON.parse(payload);
-    let removeUpdateListener;
-    let removeExplorerForward;
-    let removeSubscriptionTerminationListener;
-    let removeReloadListener;
-    let clearRequestInterval;
-    let removeExplorerListener;
+  const panel = await browser.devtools.panels.create(
+    "Apollo",
+    "logo_devtools.png",
+    "panel.html",
+  );
 
-    panel.onShown.addListener((window) => {
-      sendMessageToClient(PANEL_OPEN);
+  isPanelCreated = true;
+  const { queries, mutations, cache } = JSON.parse(payload);
+  let removeUpdateListener;
+  let removeExplorerForward;
+  let removeSubscriptionTerminationListener;
+  let removeReloadListener;
+  let clearRequestInterval;
+  let removeExplorerListener;
 
-      const {
-        __DEVTOOLS_APPLICATION__: {
-          initialize,
-          writeData,
-          receiveExplorerRequests,
-          receiveSubscriptionTerminationRequest,
-          sendResponseToExplorer,
-          handleReload,
-          handleReloadComplete,
-        },
-      } = window as any;
+  panel.onShown.addListener((window) => {
+    sendMessageToClient(PANEL_OPEN);
 
-      if (!isAppInitialized) {
-        initialize();
-        writeData({ queries, mutations, cache: JSON.stringify(cache) });
-        isAppInitialized = true;
-      }
+    const {
+      __DEVTOOLS_APPLICATION__: {
+        initialize,
+        writeData,
+        receiveExplorerRequests,
+        receiveSubscriptionTerminationRequest,
+        sendResponseToExplorer,
+        handleReload,
+        handleReloadComplete,
+      },
+    } = window as any;
 
-      clearRequestInterval = startRequestInterval();
+    if (!isAppInitialized) {
+      initialize();
+      writeData({ queries, mutations, cache: JSON.stringify(cache) });
+      isAppInitialized = true;
+    }
 
-      removeUpdateListener = devtools.listen(UPDATE, ({ payload }) => {
-        const { queries, mutations, cache } = JSON.parse(payload);
-        writeData({ queries, mutations, cache: JSON.stringify(cache) });
-      });
+    clearRequestInterval = startRequestInterval();
 
-      // Add connection so client can send to `background:devtools-${inspectedTabId}:explorer`
-      devtools.addConnection("explorer", sendResponseToExplorer);
-      removeExplorerListener = receiveExplorerRequests(({ detail }) => {
+    removeUpdateListener = devtools.listen(UPDATE, ({ payload }) => {
+      const { queries, mutations, cache } = JSON.parse(payload);
+      writeData({ queries, mutations, cache: JSON.stringify(cache) });
+    });
+
+    // Add connection so client can send to `background:devtools-${inspectedTabId}:explorer`
+    devtools.addConnection("explorer", sendResponseToExplorer);
+    removeExplorerListener = receiveExplorerRequests(({ detail }) => {
+      devtools.broadcast(detail);
+    });
+
+    removeSubscriptionTerminationListener =
+      receiveSubscriptionTerminationRequest(({ detail }) => {
         devtools.broadcast(detail);
       });
 
-      removeSubscriptionTerminationListener =
-        receiveSubscriptionTerminationRequest(({ detail }) => {
-          devtools.broadcast(detail);
-        });
+    // Forward all Explorer requests to the client
+    removeExplorerForward = devtools.forward(
+      EXPLORER_REQUEST,
+      `background:tab-${inspectedTabId}:client`
+    );
 
-      // Forward all Explorer requests to the client
-      removeExplorerForward = devtools.forward(
-        EXPLORER_REQUEST,
-        `background:tab-${inspectedTabId}:client`
-      );
+    // Listen for tab reload from background
+    removeReloadListener = devtools.listen(RELOADING_TAB, () => {
+      handleReload();
+      clearRequestInterval();
 
-      // Listen for tab reload from background
-      removeReloadListener = devtools.listen(RELOADING_TAB, () => {
-        handleReload();
-        clearRequestInterval();
-
-        const removeListener = devtools.listen(RELOAD_TAB_COMPLETE, () => {
-          clearRequestInterval = startRequestInterval();
-          handleReloadComplete();
-          removeListener();
-        });
+      const removeListener = devtools.listen(RELOAD_TAB_COMPLETE, () => {
+        clearRequestInterval = startRequestInterval();
+        handleReloadComplete();
+        removeListener();
       });
     });
+  });
 
-    panel.onHidden.addListener(() => {
-      isPanelCreated = false;
-      clearRequestInterval();
-      removeExplorerForward();
-      removeSubscriptionTerminationListener();
-      removeUpdateListener();
-      removeReloadListener();
-      removeExplorerListener();
-      devtools.removeConnection("explorer");
-      sendMessageToClient(PANEL_CLOSED);
-    });
-  }
+  panel.onHidden.addListener(() => {
+    isPanelCreated = false;
+    clearRequestInterval();
+    removeExplorerForward();
+    removeSubscriptionTerminationListener();
+    removeUpdateListener();
+    removeReloadListener();
+    removeExplorerListener();
+    devtools.removeConnection("explorer");
+    sendMessageToClient(PANEL_CLOSED);
+  });
 });
 
 sendMessageToClient(DEVTOOLS_INITIALIZED);
