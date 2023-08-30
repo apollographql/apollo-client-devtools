72292ca7b0546266f2968837d815dfbf6c23ef42
Lenz Weber-Tronic <mail@lenzw.de>
2023-08-30 19:49:03 +0200

    New Apollo Client registration mechanism (#1038)
    Co-authored-by: Alessia Bellisario <github@bellisar.io>

---
+++ .changeset/neat-seahorses-breathe.md
@@ -0,0 +1,5 @@
+---
+"apollo-client-devtools": patch
+---
+
+Make the devtools <> client registration mechanism more robust. Also lays the groundwork for registering multiple Apollo Client instances in the future.
---
+++ src/privateAccess.ts
@@ -0,0 +1,32 @@
+import type { ApolloClient, ObservableQuery } from "@apollo/client";
+import type { QueryInfo } from "@apollo/client/core/QueryInfo";
+import type { QueryManager } from "@apollo/client/core/QueryManager";
+
+type KnownPrivates = [
+  [
+    ApolloClient<any>,
+    {
+      queryManager: QueryManager<any>;
+    }
+  ],
+  [
+    ObservableQuery,
+    {
+      queryInfo: QueryInfo;
+    }
+  ]
+];
+
+export type WithPrivateAccess<U> = U extends infer T
+  ? T extends undefined
+    ? undefined
+    : KnownPrivates[number] extends infer Known
+    ? Known extends [T, infer Overrides]
+      ? Omit<T, keyof Overrides> & Overrides
+      : never
+    : never
+  : never;
+
+export function getPrivateAccess<T>(t: T): WithPrivateAccess<T> {
+  return t as any;
+}
