6352ffa81be963bd8ceb79526f15d61a80af71ea
Jerel Miller <jerelmiller@gmail.com>
2023-09-01 14:00:01 -0600

    Remove unused objectFilter function

--- src/application/components/Cache/common/__tests__/utils.test.ts
+++ src/application/components/Cache/common/__tests__/utils.test.ts
@@ -1,4 +1,4 @@
-import { getRootCacheIds, objectFilter } from "../utils";
+import { getRootCacheIds } from "../utils";
 
 describe("Utility tests", () => {
   describe("#getRootCacheIds", () => {
@@ -68,87 +68,6 @@ describe("Utility tests", () => {
       expect(cacheIds[1]).toEqual("ROOT_MUTATION");
       expect(cacheIds[2]).toEqual("ROOT_SUBSCRIPTION");
       expect(cacheIds[3]).toEqual("Result:1");
-    });
-  });
-
-  describe("#objectFilter", () => {
-    it("should return a new object with key/value pair if matching key found", () => {
-      const car = {
-        make: "Ford",
-        model: "Pinto",
-      };
-      expect(objectFilter(car, "make")).toEqual({
-        make: "Ford",
-      });
-    });
-
-    it("should return a new object with key/value pair if partial matching key found", () => {
-      const car = {
-        make: "Ford",
-        model: "Pinto",
-      };
-      expect(objectFilter(car, "ma")).toEqual({
-        make: "Ford",
-      });
-    });
-
-    it("should return a new object with key/value pair if matching string value found", () => {
-      const car = {
-        make: "Ford",
-        model: "Pinto",
-      };
-      expect(objectFilter(car, "Ford")).toEqual({
-        make: "Ford",
-      });
-    });
-
-    it("should return a new object with key/value pair if partial matching string value found", () => {
-      const car = {
-        make: "Ford",
-        model: "Pinto",
-      };
-      expect(objectFilter(car, "Fo")).toEqual({
-        make: "Ford",
-      });
-    });
-
-    it("should return all key/value pair matches in a new object", () => {
-      const car = {
-        makeName: "Ford",
-        makeId: "FRD010",
-      };
-      expect(objectFilter(car, "make")).toEqual({
-        makeName: "Ford",
-        makeId: "FRD010",
-      });
-    });
-
-    it("should return key/value matches using case insensitivity", () => {
-      const car = {
-        make: "FORD",
-        model: "Pinto",
-      };
-      expect(objectFilter(car, "ford")).toEqual({
-        make: "FORD",
-      });
-    });
-
-    it("should return undefined if no matching key/value is found", () => {
-      const car = {
-        make: "Ford",
-        model: "Pinto",
-      };
-      expect(objectFilter(car, "vin")).toBeUndefined();
-    });
-
-    it("should exclude nested objects", () => {
-      const car = {
-        make: "Ford",
-        model: {
-          id: "Pinto",
-        },
-      };
-      expect(objectFilter(car, "Pinto")).toBeUndefined();
     });
   });
 });
