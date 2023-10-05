02d0edff8fc16d9a1c3523d9206bdd22d821e95f
Alessia Bellisario <github@bellisar.io>
2023-10-05 14:27:52 -0400

    feat: adds dark mode theming to modal

--- src/application/components/Layouts/SettingsModal.tsx
+++ src/application/components/Layouts/SettingsModal.tsx
@@ -43,20 +43,20 @@ export function Modal({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full max-w-sm sm:p-6">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white dark:bg-black-base px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full max-w-sm sm:p-6">
                 <div>
                   <div className="mt-1">
                     <Dialog.Title
                       as="h3"
-                      className="text-base font-semibold leading-6 mb-6"
+                      className="text-base leading-6 mb-6 dark:text-white"
                     >
                       Settings
                     </Dialog.Title>
-                    <div className="mt-2 text-sm">
+                    <div className="mt-2 text-sm dark:text-white">
                       <p>
                         Devtools version:{" "}
                         <a
-                          className="underline text-purple-darker font-monospace"
+                          className="underline text-purple-darker dark:text-white font-monospace"
                           target="_blank"
                           rel="noopener noreferrer"
                           href={`https://github.com/apollographql/apollo-client-devtools/releases/tag/v${VERSION}`}
