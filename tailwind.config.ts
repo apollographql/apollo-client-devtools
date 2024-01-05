17187de820dec36d07a3879a00b0c50ceb2fc41a
Jerel Miller <jerelmiller@gmail.com>
2024-01-05 16:17:35 -0700

    Add font size to config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,14 @@
 import type { Config } from "tailwindcss";
-import { colors } from "@apollo/brand";
+import { colors, typography } from "@apollo/brand";
+
+function mapEntries<T, R>(
+  obj: { [key: string]: T },
+  mapper: (key: string, value: T) => R
+) {
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [key, mapper(key, value)])
+  );
+}
 
 export default {
   content: [
@@ -15,6 +24,10 @@ export default {
     backgroundColor: colors.tokens.bg,
     borderColor: colors.tokens.border,
     textColor: colors.tokens.text,
+    fontSize: mapEntries(typography.primitives.fontSize, (name, config) => [
+      `${config.fontSize}px`,
+      `${config.lineHeight}px`,
+    ]),
     extend: {},
   },
   plugins: [],
