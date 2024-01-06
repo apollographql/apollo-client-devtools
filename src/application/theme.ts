d14f58896d6c00f3c9657c71962b22a7633544f3
Jerel Miller <jerelmiller@gmail.com>
2024-01-05 22:51:59 -0700

    Remove treetheme from theme file

--- src/application/theme.ts
+++ src/application/theme.ts
@@ -61,47 +61,6 @@ export const themes: Record<ColorTheme, Record<string, ShadedColor>> = {
   },
 };
 
-// The Base16 options used below are explained here:
-// https://github.com/chriskempson/base16/blob/7fa89d33bc77a43e1cf93c4654b235e21f827ce3/styling.md
-export const treeTheme: Record<ColorTheme, Record<string, ShadedColor>> = {
-  [ColorTheme.Light]: {
-    base00: colors.white as ShadedColor,
-    base01: colors.white as ShadedColor,
-    base02: "#FFFF00" as ShadedColor,
-    base03: "#969896" as ShadedColor,
-    base04: colors.white as ShadedColor,
-    base05: colors.white as ShadedColor,
-    base06: colors.white as ShadedColor,
-    base07: colors.white as ShadedColor,
-    base08: colors.grey.base,
-    base09: colors.indigo.base,
-    base0A: colors.black.base,
-    base0B: colors.pink.dark,
-    base0C: colors.black.base,
-    base0D: colors.black.base,
-    base0E: colors.black.base,
-    base0F: colors.black.base,
-  },
-  [ColorTheme.Dark]: {
-    base00: themes[ColorTheme.Dark].main,
-    base01: colors.white as ShadedColor,
-    base02: "#FFFF00" as ShadedColor,
-    base03: "#969896" as ShadedColor,
-    base04: colors.white as ShadedColor,
-    base05: colors.white as ShadedColor,
-    base06: colors.white as ShadedColor,
-    base07: colors.white as ShadedColor,
-    base08: colors.grey.base,
-    base09: colors.indigo.light,
-    base0A: colors.white as ShadedColor,
-    base0B: colors.pink.light,
-    base0C: colors.white as ShadedColor,
-    base0D: colors.white as ShadedColor,
-    base0E: colors.white as ShadedColor,
-    base0F: colors.white as ShadedColor,
-  },
-};
-
 export type Theme = (typeof themes)[keyof typeof themes];
 export const colorTheme = makeVar<ColorTheme>(getPreferredTheme());
 export const isDarkMode = (theme: ColorTheme): boolean => {
@@ -111,14 +70,4 @@ export const isDarkMode = (theme: ColorTheme): boolean => {
 export const useTheme = (): Theme => {
   const theme = useReactiveVar(colorTheme);
   return themes[theme];
-};
-
-export const useTreeTheme = () => {
-  const theme = useReactiveVar(colorTheme);
-
-  return {
-    scheme: "apollo",
-    author: "Apollo (community@apollographql.com)",
-    ...treeTheme[theme],
-  };
 };
