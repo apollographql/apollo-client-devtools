47e8ebee2514b29a4b10b87d83d9d9e5cffc56ac
Jerel Miller <jerelmiller@gmail.com>
2024-01-08 13:08:06 -0700

    Update settings modal to use updated styles

--- src/application/components/Layouts/SettingsModal.tsx
+++ src/application/components/Layouts/SettingsModal.tsx
@@ -22,7 +22,7 @@ export function SettingsModal({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-grey-darker bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 bg-black-300/50" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
@@ -36,27 +36,25 @@ export function SettingsModal({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white dark:bg-black-base px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full max-w-sm sm:p-6">
-                <div>
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-primary dark:bg-primary-dark border border-primary dark:border-primary-dark p-6 text-left shadow-xl transition-all sm:my-8 sm:w-full max-w-sm">
+                <div className="flex flex-col gap-2">
                   <Dialog.Title
                     as="h3"
-                    className="text-base leading-6 mb-6 dark:text-white"
+                    className="prose-lg text-heading dark:text-heading-dark"
                   >
                     Settings
                   </Dialog.Title>
-                  <div className="mt-2 text-sm dark:text-white">
-                    <p>
-                      Devtools version:{" "}
-                      <a
-                        className="underline text-purple-darker dark:text-white font-monospace"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={`https://github.com/apollographql/apollo-client-devtools/releases/tag/v${VERSION}`}
-                      >
-                        {VERSION}
-                      </a>
-                    </p>
-                  </div>
+                  <p>
+                    Devtools version:{" "}
+                    <a
+                      className="font-code"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={`https://github.com/apollographql/apollo-client-devtools/releases/tag/v${VERSION}`}
+                    >
+                      {VERSION}
+                    </a>
+                  </p>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
