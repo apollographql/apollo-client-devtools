ba7dfe52a53e22fd6ae1fca6d44ce889888b0e0d
Jerel Miller <jerelmiller@gmail.com>
2024-01-08 12:35:47 -0700

    Update EntityView to use updated styles

--- src/application/components/Cache/main/EntityView.tsx
+++ src/application/components/Cache/main/EntityView.tsx
@@ -1,21 +1,8 @@
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-import { rem } from "polished";
-
 import { ReactNode } from "react";
+import { clsx } from "clsx";
+
 import { JSONObject } from "../../../types/json";
 import { JSONTreeViewer } from "../../JSONTreeViewer";
-
-const cacheStyles = css`
-  padding-top: 1rem;
-  font-family: "Source Code Pro", monospace;
-  font-size: ${rem(13)};
-
-  > ul {
-    margin-top: 0 !important;
-    margin-left: ${rem(8)} !important;
-  }
-`;
 
 interface EntityViewProps {
   cacheId: string;
@@ -27,23 +14,18 @@ export function EntityView({ cacheId, data, setCacheId }: EntityViewProps) {
   if (!data) return null;
 
   return (
-    <div css={cacheStyles}>
+    <div className="pt-4 font-code text-sm">
       {cacheId}
       <JSONTreeViewer
         data={data}
         hideRoot={true}
+        style={{ marginTop: 0 }}
         valueRenderer={(valueAsString: ReactNode, value, key) => {
           return (
             <span
-              css={css`
-                ${key === "__ref" &&
-                css`
-                  &:hover {
-                    text-decoration: underline;
-                    cursor: pointer;
-                  }
-                `}
-              `}
+              className={clsx({
+                ["hover:underline hover:cursor-pointer"]: key === "__ref",
+              })}
               onClick={() => {
                 if (key === "__ref") {
                   setCacheId(value as string);
