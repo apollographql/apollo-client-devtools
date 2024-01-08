352779b5345c5cd4e0605458e9e212b620136d58
Jerel Miller <jerelmiller@gmail.com>
2024-01-08 16:25:45 -0700

    Update MutationViewer to newer code block styles

--- src/application/components/Mutations/MutationViewer.tsx
+++ src/application/components/Mutations/MutationViewer.tsx
@@ -7,6 +7,7 @@ import { fragmentRegistry } from "../../fragmentRegistry";
 import { gql } from "@apollo/client";
 import { MutationViewer_mutation as WatchedMutation } from "../../types/gql";
 import { JSONTreeViewer } from "../JSONTreeViewer";
+import { Tabs } from "../Tabs";
 
 interface MutationViewerProps {
   mutation: WatchedMutation;
@@ -21,41 +22,34 @@ fragmentRegistry.register(gql`
 
 export const MutationViewer = ({ mutation }: MutationViewerProps) => {
   return (
-    <div className="pt-3 grid [grid-template-columns:minmax(12rem,2fr)_minmax(12rem,1fr)] [grid-template-rows:1.75rem_auto] gap-x-6 [grid-template-areas:'queryStringHeader_queryDataHeader'_'queryStringMain_queryDataMain']">
-      <h4 className="[grid-area:queryStringHeader] flex justify-between m-0 text-sm font-bold border-b border-b-primary dark:border-b-primary-dark">
-        Mutation String
-        <CopyToClipboard text={mutation.mutationString}>
-          <IconCopy
-            className="ml-auto !h-4 cursor-pointer text-secondary dark:text-secondary-dark hover:text-primary hover:dark:text-primary-dark"
-            data-testid="copy-mutation-string"
-          />
-        </CopyToClipboard>
-      </h4>
-      <CodeBlock
-        className="[grid-area:queryStringMain] mt-4 text-sm h-full overflow-y-hidden"
-        code={mutation.mutationString}
-        language="graphql"
-      />
+    <div className="pt-3 grid [grid-template-columns:1fr_350px] gap-6">
       <div>
-        <div className="flex [grid-area:queryDataHeader] h-full text-sm font-semibold border-b-primary dark:border-b-primary-dark border-b">
-          <span className="px-2 pb-2 text-white border-b-focused dark:border-b-focused-dark border-b">
-            Variables
-          </span>
+        <CodeBlock
+          className="overflow-y-hidden"
+          code={mutation.mutationString}
+          language="graphql"
+        />
+      </div>
+      <Tabs defaultValue="variables">
+        <Tabs.List>
+          <Tabs.Trigger value="variables">Variables</Tabs.Trigger>
           <CopyToClipboard text={JSON.stringify(mutation.variables)}>
             <IconCopy
               className="ml-auto !h-4 cursor-pointer text-secondary dark:text-secondary-dark hover:text-primary hover:dark:text-primary-dark"
               data-testid="copy-mutation-variables"
             />
           </CopyToClipboard>
+        </Tabs.List>
+        <div className="mt-4 pb-4 text-sm">
+          <Tabs.Content value="variables">
+            <JSONTreeViewer
+              className="[&>li]:!pt-0"
+              style={{ marginTop: 0 }}
+              data={mutation.variables}
+            />
+          </Tabs.Content>
         </div>
-        <div className="[grid-area:queryDataMain] mt-4 pb-4 text-sm">
-          <JSONTreeViewer
-            className="[&>li]:!pt-0"
-            style={{ marginTop: 0 }}
-            data={mutation.variables}
-          />
-        </div>
-      </div>
+      </Tabs>
     </div>
   );
 };
