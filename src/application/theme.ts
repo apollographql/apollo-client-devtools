954ecaf6a5d0142d26cca6f027b3cd6d64e04f11
Jerel Miller <jerelmiller@gmail.com>
2024-01-09 22:48:28 -0700

    Remove old theme colors from theme file

--- src/application/theme.ts
+++ src/application/theme.ts
@@ -1,6 +1,4 @@
-import { rgba } from "polished";
-import { makeVar, useReactiveVar } from "@apollo/client";
-import { colors, ShadedColor } from "@apollo/space-kit/colors";
+import { makeVar } from "@apollo/client";
 
 export enum ColorTheme {
   Light = "light",
@@ -37,37 +35,4 @@ export function listenForThemeChange(
   return () => window.matchMedia(Mode.Dark).removeEventListener("change", cb);
 }
 
-const shared = {
-  sidebarSelected: colors.blilet.base,
-  whiteTransparent: rgba(colors.white, 0.5) as ShadedColor,
-};
-
-export const themes: Record<ColorTheme, Record<string, ShadedColor>> = {
-  [ColorTheme.Light]: {
-    primary: colors.blilet.darker,
-    main: colors.white as ShadedColor,
-    sidebarHover: colors.blilet.darkest,
-    mainBorder: colors.silver.darker,
-    textPrimary: colors.black.base,
-    ...shared,
-  },
-  [ColorTheme.Dark]: {
-    primary: "#1a1c22" as ShadedColor,
-    main: "#23262d" as ShadedColor,
-    sidebarHover: rgba(colors.grey.dark, 0.4) as ShadedColor,
-    mainBorder: colors.black.lighter,
-    textPrimary: colors.white as ShadedColor,
-    ...shared,
-  },
-};
-
-export type Theme = (typeof themes)[keyof typeof themes];
 export const colorTheme = makeVar<ColorTheme>(getPreferredTheme());
-export const isDarkMode = (theme: ColorTheme): boolean => {
-  return theme === ColorTheme.Dark;
-};
-
-export const useTheme = (): Theme => {
-  const theme = useReactiveVar(colorTheme);
-  return themes[theme];
-};
