b5303c31ce5e2ce318f180056ac5065a155be943
Jerel Miller <jerelmiller@gmail.com>
2024-01-09 18:09:43 -0700

    Create a ButtonGroup component

---
+++ src/application/components/ButtonGroup.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+interface ButtonGroupProps {
+  children: ReactNode;
+}
+
+export function ButtonGroup({ children }: ButtonGroupProps) {
+  return <div className="flex gap-2 items-center">{children}</div>;
+}
