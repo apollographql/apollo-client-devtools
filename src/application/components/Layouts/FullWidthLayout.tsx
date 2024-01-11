28fb03717a6f25c519b0b0b3f9df98719ffcfeba
Jerel Miller <jerelmiller@gmail.com>
2024-01-11 10:34:19 -0700

    Remove settings modal from full width layout

--- src/application/components/Layouts/FullWidthLayout.tsx
+++ src/application/components/Layouts/FullWidthLayout.tsx
@@ -1,9 +1,5 @@
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import { clsx } from "clsx";
-
-import { SettingsModal } from "./SettingsModal";
-import { Button } from "../Button";
-import { SettingsIcon } from "../icons/Settings";
 
 interface FullWidthLayoutProps {
   children: ReactNode;
@@ -24,21 +20,9 @@ interface HeaderProps {
 }
 
 const Header = ({ children, className }: HeaderProps) => {
-  const [open, setOpen] = useState(false);
-
   return (
     <div className={className} data-testid="header">
       {children}
-      <Button
-        className="ml-auto"
-        size="sm"
-        variant="hidden"
-        onClick={() => setOpen(true)}
-      >
-        <SettingsIcon aria-hidden="true" />
-        <span className="sr-only">Settings</span>
-      </Button>
-      <SettingsModal open={open} onOpen={setOpen} />
     </div>
   );
 };
