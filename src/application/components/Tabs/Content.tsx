184efd1b4c1a58674dd34b3188321353b3bff646
Jerel Miller <jerelmiller@gmail.com>
2024-01-11 09:11:03 -0700

    Add additional supported props to tab content

--- src/application/components/Tabs/Content.tsx
+++ src/application/components/Tabs/Content.tsx
@@ -2,10 +2,21 @@ import { ReactNode } from "react";
 import { Content as ContentBase } from "@radix-ui/react-tabs";
 
 interface ContentProps {
+  asChild?: boolean;
   children: ReactNode;
+  forceMount?: true;
   value: string;
 }
 
-export function Content({ children, value }: ContentProps) {
-  return <ContentBase value={value}>{children}</ContentBase>;
+export function Content({
+  asChild,
+  children,
+  forceMount,
+  value,
+}: ContentProps) {
+  return (
+    <ContentBase asChild={asChild} forceMount={forceMount} value={value}>
+      {children}
+    </ContentBase>
+  );
 }
