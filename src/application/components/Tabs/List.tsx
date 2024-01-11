6f22c7d780cd30b9c18aed1bb2a04ad966baddf9
Jerel Miller <jerelmiller@gmail.com>
2024-01-11 09:11:15 -0700

    Allow className on tab list

--- src/application/components/Tabs/List.tsx
+++ src/application/components/Tabs/List.tsx
@@ -1,13 +1,20 @@
 import { List as ListBase } from "@radix-ui/react-tabs";
 import { ReactNode } from "react";
+import { clsx } from "clsx";
 
 interface ListProps {
+  className?: string;
   children: ReactNode;
 }
 
-export function List({ children }: ListProps) {
+export function List({ className, children }: ListProps) {
   return (
-    <ListBase className="flex items-center gap-6 border-b border-b-primary dark:border-b-primary-dark">
+    <ListBase
+      className={clsx(
+        className,
+        "flex items-center gap-6 border-b border-b-primary dark:border-b-primary-dark"
+      )}
+    >
       {children}
     </ListBase>
   );
