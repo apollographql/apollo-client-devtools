0325461ee8fdd353b91356001585b615ff7960d5
Jerel Miller <jerelmiller@gmail.com>
2024-01-11 10:20:39 -0700

    Remove support for asChild in tabs content

--- src/application/components/Tabs/Content.tsx
+++ src/application/components/Tabs/Content.tsx
@@ -2,20 +2,14 @@ import { ReactNode } from "react";
 import { Content as ContentBase } from "@radix-ui/react-tabs";
 
 interface ContentProps {
-  asChild?: boolean;
   children: ReactNode;
   forceMount?: true;
   value: string;
 }
 
-export function Content({
-  asChild,
-  children,
-  forceMount,
-  value,
-}: ContentProps) {
+export function Content({ children, forceMount, value }: ContentProps) {
   return (
-    <ContentBase asChild={asChild} forceMount={forceMount} value={value}>
+    <ContentBase forceMount={forceMount} value={value}>
       {children}
     </ContentBase>
   );
