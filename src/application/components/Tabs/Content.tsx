d9b83d62b55d64e33ca99f36b51b50f8b36ba565
Jerel Miller <jerelmiller@gmail.com>
2024-01-11 10:43:33 -0700

    Allow className on tabs content

--- src/application/components/Tabs/Content.tsx
+++ src/application/components/Tabs/Content.tsx
@@ -2,14 +2,20 @@ import { ReactNode } from "react";
 import { Content as ContentBase } from "@radix-ui/react-tabs";
 
 interface ContentProps {
+  className?: string;
   children: ReactNode;
   forceMount?: true;
   value: string;
 }
 
-export function Content({ children, forceMount, value }: ContentProps) {
+export function Content({
+  className,
+  children,
+  forceMount,
+  value,
+}: ContentProps) {
   return (
-    <ContentBase forceMount={forceMount} value={value}>
+    <ContentBase className={className} forceMount={forceMount} value={value}>
       {children}
     </ContentBase>
   );
