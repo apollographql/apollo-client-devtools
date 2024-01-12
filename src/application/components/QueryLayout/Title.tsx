1f6061425951cd8f1e51ebed051777b0a91e4413
Jerel Miller <jerelmiller@gmail.com>
2024-01-12 09:27:17 -0700

    Add test id for title

--- src/application/components/QueryLayout/Title.tsx
+++ src/application/components/QueryLayout/Title.tsx
@@ -6,7 +6,10 @@ interface TitleProps {
 
 export function Title({ children }: TitleProps) {
   return (
-    <h1 className="prose-2xl text-heading dark:text-heading-dark">
+    <h1
+      data-testid="title"
+      className="prose-2xl text-heading dark:text-heading-dark"
+    >
       <code>{children}</code>
     </h1>
   );
