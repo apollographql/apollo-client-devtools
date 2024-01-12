03c4ab22ef97c55f57c319dcdc3557d3efb61c04
Jerel Miller <jerelmiller@gmail.com>
2024-01-12 14:45:26 -0700

    Only apply max height on code block for narrow screens

--- src/application/components/QueryLayout/QueryString.tsx
+++ src/application/components/QueryLayout/QueryString.tsx
@@ -6,7 +6,10 @@ interface QueryStringProps {
 
 export function QueryString({ code }: QueryStringProps) {
   return (
-    <div data-testid="query" className="[grid-area:content] max-h-[500px]">
+    <div
+      data-testid="query"
+      className="[grid-area:content] max-h-[500px] lg:max-h-none"
+    >
       <CodeBlock language="graphql" code={code} className="max-h-full" />
     </div>
   );
