424239f2be44b08c9c4f68a0830b564d1eb961c2
Jerel Miller <jerelmiller@gmail.com>
2024-01-12 10:50:02 -0700

    Update SidebarLayout tests

--- src/application/components/Layouts/__tests__/SidebarLayout.test.tsx
+++ src/application/components/Layouts/__tests__/SidebarLayout.test.tsx
@@ -5,24 +5,17 @@ import { renderWithApolloClient } from "../../../utilities/testing/renderWithApo
 import { SidebarLayout } from "../SidebarLayout";
 
 describe("<SidebarLayout />", () => {
-  const navigationProps = { queriesCount: 0, mutationsCount: 0 };
-
   it("renders", () => {
-    const { container } = renderWithApolloClient(
-      <SidebarLayout navigationProps={navigationProps}>
-        <SidebarLayout.Sidebar navigationProps={navigationProps}>
+    renderWithApolloClient(
+      <SidebarLayout>
+        <SidebarLayout.Sidebar>
           This is the sidebar section
         </SidebarLayout.Sidebar>
-        <SidebarLayout.Header>This is the header section</SidebarLayout.Header>
         <SidebarLayout.Main>This is the main section</SidebarLayout.Main>
       </SidebarLayout>
     );
-    // eslint-disable-next-line testing-library/no-container
-    expect(container.querySelector("nav")).toBeInTheDocument();
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
     expect(screen.getByText("This is the sidebar section")).toBeInTheDocument();
-    expect(screen.getByTestId("header")).toBeInTheDocument();
-    expect(screen.getByText("This is the header section")).toBeInTheDocument();
     expect(screen.getByTestId("main")).toBeInTheDocument();
     expect(screen.getByText("This is the main section")).toBeInTheDocument();
   });
