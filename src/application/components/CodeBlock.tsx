9e5de1101af88e89da0814a2d7673afec4215bab
Jerel Miller <jerelmiller@gmail.com>
2024-01-25 09:45:51 -0700

    Fix formatting

--- src/application/components/CodeBlock.tsx
+++ src/application/components/CodeBlock.tsx
@@ -4,7 +4,7 @@ import { colors } from "@apollo/brand";
 import { clsx } from "clsx";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { ColorTheme, colorTheme } from "../theme";
-import IconCopy from '@apollo/icons/default/IconCopy.svg'
+import IconCopy from "@apollo/icons/default/IconCopy.svg";
 import { Button } from "./Button";
 import { useMemo } from "react";
 
