bbe48541b8c4a4098179edd2cc50d0c4ddfaec06
Jerel Miller <jerelmiller@gmail.com>
2024-02-07 13:19:36 -0700

    Remove outdated test

--- src/application/__tests__/App.test.tsx
+++ src/application/__tests__/App.test.tsx
@@ -3,7 +3,7 @@ import { screen, waitFor } from "@testing-library/react";
 
 import { renderWithApolloClient } from "../utilities/testing/renderWithApolloClient";
 import { currentScreen, Screens } from "../components/Layouts/Navigation";
-import { App, reloadStatus } from "../App";
+import { App } from "../App";
 
 jest.mock("../components/Explorer/Explorer", () => ({
   Explorer: () => <div>Build</div>,
@@ -23,26 +23,5 @@ describe("<App />", () => {
       expect(currentScreen()).toEqual(Screens.Explorer);
     });
     await screen.findByText("Build");
-  });
-
-  test("after reload, renders the Queries screen", async () => {
-    currentScreen(Screens.Mutations);
-    renderWithApolloClient(<App />);
-    const element = screen.getByText("Mutations (0)");
-    expect(element).toBeInTheDocument();
-    await waitFor(() => {
-      reloadStatus(true);
-      expect(reloadStatus()).toBeTruthy();
-    });
-    await waitFor(() => {
-      expect(element).not.toBeInTheDocument();
-    });
-    await waitFor(() => {
-      reloadStatus(false);
-      expect(reloadStatus()).toBeFalsy();
-    });
-    await waitFor(() => {
-      expect(screen.getByText("Queries (0)")).toBeInTheDocument();
-    });
   });
 });
