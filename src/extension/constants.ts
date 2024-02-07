406843ae0372604d5217f26245e0ceb868ae533f
Jerel Miller <jerelmiller@gmail.com>
2024-02-07 15:04:07 -0700

    Merge constants into one block

--- src/extension/constants.ts
+++ src/extension/constants.ts
@@ -1,13 +1,12 @@
 export const REQUEST_TAB_ID = "request-tab-id";
+export const CLIENT_NOT_FOUND = "client-not-found";
 export const CONNECT_TO_CLIENT = "connect-to-client";
+export const CONNECT_TO_CLIENT_TIMEOUT = "connect-to-client-timeout";
+export const CONNECT_TO_DEVTOOLS = "connect-to-devtools";
+export const DEVTOOLS_STATE_CHANGED = "devtools-state-changed";
+export const DISCONNECT_FROM_DEVTOOLS = "disconnect-from-devtools";
+export const INITIALIZE_PANEL = "initialize-panel";
 export const REQUEST_DATA = "request-data";
 export const UPDATE = "update";
 export const EXPLORER_REQUEST = "explorer-request";
 export const EXPLORER_RESPONSE = "explorer-response";
-
-export const CLIENT_NOT_FOUND = "client-not-found";
-export const CONNECT_TO_DEVTOOLS = "connect-to-devtools";
-export const CONNECT_TO_CLIENT_TIMEOUT = "connect-to-client-timeout";
-export const DEVTOOLS_STATE_CHANGED = "devtools-state-changed";
-export const DISCONNECT_FROM_DEVTOOLS = "disconnect-from-devtools";
-export const INITIALIZE_PANEL = "initialize-panel";
