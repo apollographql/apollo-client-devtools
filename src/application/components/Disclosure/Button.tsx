4a1a8cc1cbdbb7d63d9f918cf91823724a16c6d3
Jerel Miller <jerelmiller@gmail.com>
2024-02-07 17:13:39 -0700

    Add Disclosure component

---
+++ src/application/components/Disclosure/Button.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from "react";
+import { Disclosure } from "@headlessui/react";
+import IconChevronUp from "@apollo/icons/default/IconChevronUp.svg";
+import clsx from "clsx";
+
+interface DisclosureButtonProps {
+  children: ReactNode;
+}
+
+export function DisclosureButton({ children }: DisclosureButtonProps) {
+  return (
+    <Disclosure.Button
+      className={clsx(
+        "bg-primary dark:bg-primary-dark border border-primary dark:border-primary-dark py-3 px-2 hover:bg-secondary hover:dark:bg-secondary-dark transition-colors text-left w-full font-heading text-heading dark:text-heading-dark font-medium text-md flex gap-2 items-center",
+        "rounded-t ui-not-open:rounded-b"
+      )}
+    >
+      {({ open }) => {
+        return (
+          <>
+            <IconChevronUp
+              className={clsx("w-4 transition-transform", {
+                "rotate-180": open,
+              })}
+            />
+            <span>{children}</span>
+          </>
+        );
+      }}
+    </Disclosure.Button>
+  );
+}
---
+++ src/application/components/Disclosure/Disclosure.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import { Disclosure as DisclosureRoot } from "@headlessui/react";
+import { DisclosureButton } from "./Button";
+import { DisclosurePanel } from "./Panel";
+
+interface DisclosureProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export function Disclosure({ children, className }: DisclosureProps) {
+  return (
+    <DisclosureRoot>
+      <div className={className}>{children}</div>
+    </DisclosureRoot>
+  );
+}
+
+Disclosure.Button = DisclosureButton;
+Disclosure.Panel = DisclosurePanel;
