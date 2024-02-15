663c4025a64638e662b842529086e087b04b935e
Jerel Miller <jerelmiller@gmail.com>
2024-02-15 09:56:06 -0700

    Import the background script in the service worker to avoid duplication

--- src/extension/service_worker/service_worker.ts
+++ src/extension/service_worker/service_worker.ts
@@ -1,26 +1,3 @@
-import Relay from "../../Relay";
-import { REQUEST_TAB_ID } from "../constants";
-import browser from "webextension-polyfill";
-
-// This sends the tab id to the inspected tab.
-browser.runtime.onMessage.addListener(({ message }, sender) => {
-  if (message === REQUEST_TAB_ID) {
-    return Promise.resolve(sender.tab?.id);
-  }
-});
-
-const background = new Relay();
-
-browser.runtime.onConnect.addListener((port) => {
-  background.addConnection(port.name, (message) => {
-    port.postMessage(message);
-  });
-
-  port.onMessage.addListener((message) => {
-    background.broadcast(message);
-  });
-
-  port.onDisconnect.addListener((port) => {
-    background.removeConnection(port.name);
-  });
-});
+// We don't use any service worker specific APIs in our background script, so
+// for now we're just using it as-is.
+import "../background/background";
