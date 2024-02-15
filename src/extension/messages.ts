c4f75cff697dee6435baa30238746c57fe40eb53
Jerel Miller <jerelmiller@gmail.com>
2024-02-14 23:59:36 -0700

    Add more messages to types

--- src/extension/messages.ts
+++ src/extension/messages.ts
@@ -1,11 +1,19 @@
+import { ExplorerResponse } from "../types";
+
 export type ApolloClientDevtoolsMessage = {
   source: "apollo-client-devtools";
   message: Message;
 };
 
 export type Message =
+  | { type: "clientNotFound" }
   | { type: "connectToClient" }
+  | { type: "connectToClientTimeout" }
   | { type: "connectToDevtools"; payload: string }
+  | { type: "disconnectFromDevtools" }
+  | { type: "explorerRequest"; payload: string }
+  | { type: "explorerResponse"; payload: ExplorerResponse }
+  | { type: "requestData" }
   | { type: "update"; payload: string };
 
 export function isApolloClientDevtoolsMessage(
