496918c7be35d05e23f9bb008a8eecced60d31d3
Jerel Miller <jerelmiller@gmail.com>
2024-02-16 12:28:57 -0700

    Remove unused listenForResponse

--- src/application/components/Explorer/explorerRelay.ts
+++ src/application/components/Explorer/explorerRelay.ts
@@ -1,30 +1,7 @@
 import Relay from "../../../Relay";
-import { ExplorerResponse, QueryResult } from "../../../types";
-import { EXPLORER_SUBSCRIPTION_TERMINATION } from "./postMessageHelpers";
+import { ExplorerResponse } from "../../../types";
 
 const explorer = new Relay();
-
-export const listenForResponse = (
-  cb: (result: QueryResult) => void,
-  operationName?: string,
-  isSubscription?: boolean
-): void => {
-  const removeListener = explorer.listen<QueryResult>(
-    `explorer:response:${operationName}`,
-    ({ payload }) => {
-      cb(payload);
-      // Queries and Mutation can be closed after a single response comes back,
-      // but we need to listen until we are told to stop for Subscriptions
-      if (!isSubscription) {
-        removeListener();
-      } else {
-        explorer.listen(EXPLORER_SUBSCRIPTION_TERMINATION, () => {
-          removeListener();
-        });
-      }
-    }
-  );
-};
 
 export const sendResponseToExplorer = ({
   payload,
