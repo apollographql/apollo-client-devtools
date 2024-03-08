d6eb6260cc23922f3b96a9377325578288b831e0
Jerel Miller <jerelmiller@gmail.com>
2024-03-07 18:27:17 -0700

    Add better UI for reactive vars

--- src/application/components/ReactiveVars.tsx
+++ src/application/components/ReactiveVars.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getPanelActor } from "../../extension/devtools/panelActor";
 import {
   reactiveVarsVar,
@@ -7,6 +7,10 @@ import {
 } from "../index";
 import { useReactiveVar } from "@apollo/client";
 import { JSONTreeViewer } from "./JSONTreeViewer";
+import { SidebarLayout } from "./Layouts/SidebarLayout";
+import { List } from "./List";
+import { ListItem } from "./ListItem";
+import { EmptyMessage } from "./EmptyMessage";
 
 const panelWindow = getPanelActor(window);
 
@@ -24,6 +28,7 @@ function subscribetoUpdates() {
 
 export function ReactiveVars() {
   const vars = useReactiveVar(reactiveVarsVar);
+  const [selected, setSelected] = useState<number | undefined>(vars[0]?.id);
 
   useEffect(() => {
     panelWindow.send({ type: "getReactiveVars" });
@@ -36,22 +41,47 @@ export function ReactiveVars() {
   useEffect(subscribeToRegistration, []);
   useEffect(subscribetoUpdates, []);
 
+  const selectedVar = vars.find((rv) => rv.id === selected);
+
   return (
-    <ul>
-      {vars.map((rv) => {
-        return (
-          <li key={rv.id}>
-            {rv.displayName || "(anonymous)"}
-            <JSONTreeViewer
-              hideRoot
-              data={rv.value}
-              labelRenderer={(keyPath) =>
-                keyPath.length === 0 ? null : <span>{keyPath[0]}:</span>
-              }
-            />
-          </li>
-        );
-      })}
-    </ul>
+    <SidebarLayout>
+      <SidebarLayout.Sidebar>
+        <List className="h-full">
+          {vars.map((rv) => {
+            return (
+              <ListItem
+                key={rv.id}
+                selected={selectedVar?.id === rv.id}
+                onClick={() => setSelected(rv.id)}
+              >
+                {rv.displayName || "(anonymous)"}
+              </ListItem>
+            );
+          })}
+        </List>
+      </SidebarLayout.Sidebar>
+      <SidebarLayout.Main className="flex flex-col gap-2 !overflow-y-auto !overflow-x-hidden">
+        {selectedVar ? (
+          <>
+            <header>
+              <h1 className="font-code font-medium text-2xl text-heading dark:text-heading-dark">
+                {selectedVar.displayName || "(anonymous)"}
+              </h1>
+            </header>
+            <div>
+              <JSONTreeViewer
+                hideRoot
+                data={selectedVar.value}
+                labelRenderer={(keyPath) =>
+                  keyPath.length === 0 ? null : <span>{keyPath[0]}:</span>
+                }
+              />
+            </div>
+          </>
+        ) : (
+          <EmptyMessage className="m-auto mt-20" />
+        )}
+      </SidebarLayout.Main>
+    </SidebarLayout>
   );
 }
