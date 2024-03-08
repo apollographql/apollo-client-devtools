46826e50ed950b8d5dbb7a982661a7669ba568cd
Jerel Miller <jerelmiller@gmail.com>
2024-03-07 17:20:25 -0700

    Add var that registers after 10 seconds

--- development/client/src/vars.js
+++ development/client/src/vars.js
@@ -1,3 +1,7 @@
 import { makeVar } from "@apollo/client";
 
 export const counterVar = makeVar(0, { displayName: "counter" });
+
+setTimeout(() => {
+  makeVar("first", { displayName: "dummy" });
+}, 10_000);
