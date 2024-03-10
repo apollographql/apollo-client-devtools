b4d25e14be0ee69da0573df12b2e3ca4c207b275
Jerel Miller <jerelmiller@gmail.com>
2024-03-09 23:10:26 -0700

    Get TS types in place for rpc

--- src/extension/__tests__/rpc.test.ts
+++ src/extension/__tests__/rpc.test.ts
@@ -1,6 +1,7 @@
-import { createRPCProvider, createRPCHandler } from "../rpc";
+import { SafeAny } from "../../types";
+import { RPC, createRpcClient, createRpcHandler } from "../rpc";
 
-function createTestAdapter<Messages = unknown>() {
+function createTestAdapter() {
   let listener: ((message: unknown) => void) | null;
   const removeListener = jest.fn(() => {
     listener = null;
@@ -11,7 +12,7 @@ function createTestAdapter<Messages = unknown>() {
     simulatePlainMessage: (message: unknown) => {
       listener?.(message);
     },
-    simulateDevtoolsMessage: (message: Messages) => {
+    simulateDevtoolsMessage: (message: Record<string, SafeAny>) => {
       listener?.({ source: "apollo-client-devtools", message });
     },
     addListener: jest.fn((fn) => {
@@ -24,29 +25,21 @@ function createTestAdapter<Messages = unknown>() {
 }
 
 test("can send and receive rpc messages", async () => {
-  type Message = { type: "add"; x: number; y: number };
-  const adapter = createTestAdapter<Message>();
-  const provider = createRPCProvider(adapter);
-  const handler = createRPCHandler(adapter);
+  type Message = RPC<"add", { x: number; y: number }, number>;
+  const adapter = createTestAdapter();
+  const client = createRpcClient<Message>(adapter);
+  const handler = createRpcHandler<Message>(adapter);
 
-  const promise = provider.send({ type: "add", x: 1, y: 2 });
+  const promise = client.request("add", { x: 1, y: 2 });
 
   expect(adapter.postMessage).toHaveBeenCalledTimes(1);
   expect(adapter.postMessage).toHaveBeenCalledWith({
     source: "apollo-client-devtools",
-    id: expect.any(Number),
-    message: { type: "add", x: 1, y: 2 },
+    id: 1,
+    message: { type: "add", params: { x: 1, y: 2 } },
   });
 
-  // const id = adapter.postMessage.mock.calls[0][0].id;
-
-  handler.handle("add", ({ x, y }) => x + y);
-
-  // adapter.simulateDevtoolsMessage({
-  //   source: "apollo-client-devtools",
-  //   id,
-  //   result: 3,
-  // });
+  handler.on("add", ({ x, y }) => x + y);
 
   await expect(promise).resolves.toBe(3);
 });
--- src/extension/rpc.ts
+++ src/extension/rpc.ts
@@ -1,26 +1,43 @@
+import { SafeAny } from "../types";
 import { MessageAdapter } from "./messageAdapters";
-import type { MessageFormat } from "./messages";
 
-export interface RPCProvider {
-  send: (message: unknown) => Promise<unknown>;
+type RPCParams = Record<string, unknown>;
+
+export type RPC<Name extends string, Params extends RPCParams, ReturnType> = {
+  __name: Name;
+  __params: Params;
+  __returnType: ReturnType;
+};
+
+export interface RpcClient<Messages extends RPC<string, RPCParams, SafeAny>> {
+  request: <TName extends Messages["__name"]>(
+    name: TName,
+    params: Extract<Messages, { __name: TName }>["__params"]
+  ) => Promise<Extract<Messages, { __name: TName }>["__returnType"]>;
 }
 
-export interface RPCHandler {
-  resolve: (
-    name: string,
-    callback: (message: unknown) => unknown
-  ) => () => unknown;
+export interface RpcHandler<Messages extends RPC<string, RPCParams, SafeAny>> {
+  on: <TName extends Messages["__name"]>(
+    name: TName,
+    callback: (
+      params: Extract<Messages, { __name: TName }>["__params"]
+    ) => Extract<Messages, { __name: TName }>["__returnType"]
+  ) => () => void;
 }
 
-export function createRPCProvider(adapter: MessageAdapter): RPCProvider {
+export function createRpcClient<
+  Messages extends RPC<string, RPCParams, SafeAny>,
+>(adapter: MessageAdapter): RpcClient<Messages> {
   return {
-    send: () => Promise.resolve(),
+    request: () => Promise.resolve(),
   };
 }
 
-export function createRPCHandler(adapter: MessageAdapter): RPCHandler {
+export function createRpcHandler<
+  Messages extends RPC<string, RPCParams, SafeAny>,
+>(adapter: MessageAdapter): RpcHandler<Messages> {
   return {
-    resolve: () => {
+    on: () => {
       return () => {};
     },
   };
