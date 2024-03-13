3f31768588d78422054fe4943bb554bbd8868776
Jerel Miller <jerelmiller@gmail.com>
2024-03-13 00:06:55 -0600

    Apply cache serialization fix to rpc call

--- src/extension/tab/hook.ts
+++ src/extension/tab/hook.ts
@@ -96,32 +96,30 @@ function initializeHook() {
     }
   });
 
-  handleRpc("getClientOperations", () => {
-    return {
-      queries: hook.getQueries(),
-      mutations: hook.getMutations(),
-      cache: hook.getCache(),
-    };
-  });
+  function getClientData() {
+    // We need to JSON stringify the data here in case the cache contains
+    // references to irregular data such as `URL` instances which are not
+    // cloneable via `structuredClone` (which `window.postMessage` uses to
+    // send messages). `JSON.stringify` does however serialize `URL`s into
+    // strings properly, so this should ensure that the cache data will be
+    // sent without errors.
+    //
+    // https://github.com/apollographql/apollo-client-devtools/issues/1258
+    return JSON.parse(
+      JSON.stringify({
+        queries: hook.getQueries(),
+        mutations: hook.getMutations(),
+        cache: hook.getCache(),
+      })
+    ) as { queries: QueryInfo[]; mutations: QueryInfo[]; cache: JSONObject };
+  }
+
+  handleRpc("getClientOperations", getClientData);
 
   function sendHookDataToDevTools(eventName: "connectToDevtools") {
     tab.send({
       type: eventName,
-      // We need to JSON stringify the data here in case the cache contains
-      // references to irregular data such as `URL` instances which are not
-      // cloneable via `structuredClone` (which `window.postMessage` uses to
-      // send messages). `JSON.stringify` does however serialize `URL`s into
-      // strings properly, so this should ensure that the cache data will be
-      // sent without errors.
-      //
-      // https://github.com/apollographql/apollo-client-devtools/issues/1258
-      payload: JSON.parse(
-        JSON.stringify({
-          queries: hook.getQueries(),
-          mutations: hook.getMutations(),
-          cache: hook.getCache(),
-        })
-      ) as { queries: QueryInfo[]; mutations: QueryInfo[]; cache: JSONObject },
+      payload: getClientData(),
     });
   }
 
