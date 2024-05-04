3b16a0a97f19663d78019b6824ec194741ecd63b
Jerel Miller <jerelmiller@gmail.com>
2024-05-03 18:28:48 -0600

    Allow ButtonGroup to take a className

--- src/application/components/ButtonGroup.tsx
+++ src/application/components/ButtonGroup.tsx
@@ -1,9 +1,13 @@
 import type { ReactNode } from "react";
+import clsx from "clsx";
 
 interface ButtonGroupProps {
+  className?: string;
   children: ReactNode;
 }
 
-export function ButtonGroup({ children }: ButtonGroupProps) {
-  return <div className="flex gap-2 items-center">{children}</div>;
+export function ButtonGroup({ className, children }: ButtonGroupProps) {
+  return (
+    <div className={clsx("flex gap-2 items-center", className)}>{children}</div>
+  );
 }
