33de534fa1725b9c756216ca1184faab027341a6
Jerel Miller <jerelmiller@gmail.com>
2024-05-16 16:25:39 -0600

    Get information about snapshot release from PR

--- src/application/components/GitHubReleaseHoverCard.tsx
+++ src/application/components/GitHubReleaseHoverCard.tsx
@@ -1,10 +1,16 @@
-import { useEffect, useReducer, type ReactNode } from "react";
+import type { ReactNode } from "react";
 import { HoverCard } from "./HoverCard";
 import { Badge } from "./Badge";
 import { Markdown } from "./Markdown";
 import { Spinner } from "./Spinner";
 import IconOutlink from "@apollo/icons/small/IconOutlink.svg";
 import IconGitHub from "@apollo/icons/default/IconGitHubSolid.svg";
+import {
+  isSnapshotRelease,
+  parseSnapshotRelease,
+  parseSnapshotTimestamp,
+} from "../utilities/github";
+import { useGitHubApi } from "../hooks/useGitHubAPI";
 
 interface GitHubReleaseHoverCardProps {
   children?: ReactNode;
@@ -15,71 +21,26 @@ export function GitHubReleaseHoverCard({
   children,
   version,
 }: GitHubReleaseHoverCardProps) {
-  if (version.startsWith("0.0.0")) {
-    return children;
-  }
-
   return (
     <HoverCard openDelay={0}>
       <HoverCard.Trigger asChild>{children}</HoverCard.Trigger>
       <HoverCard.Content>
-        <CardContents version={version} />
+        {isSnapshotRelease(version) ? (
+          <SnapshotCardContents version={version} />
+        ) : (
+          <ReleaseCardContents version={version} />
+        )}
       </HoverCard.Content>
     </HoverCard>
   );
 }
 
-type ReleaseState =
-  | { status: "pending"; data: null }
-  | { status: "success"; data: ReleaseData }
-  | { status: "error"; data: null };
-
-type Action =
-  | { type: "load" }
-  | { type: "success"; payload: ReleaseData }
-  | { type: "failed" };
-
-function reducer(state: ReleaseState, action: Action): ReleaseState {
-  switch (action.type) {
-    case "load":
-      return { status: "pending", data: null };
-    case "success":
-      return { status: "success", data: action.payload };
-    case "failed":
-      return { status: "error", data: null };
-    default:
-      return state;
-  }
-}
-
-function CardContents({ version }: { version: string }) {
-  const [{ status, data }, dispatch] = useReducer(reducer, {
-    status: "pending",
-    data: null,
-  });
-
-  useEffect(() => {
-    let ignored = false;
-    dispatch({ type: "load" });
-    getGitHubRelease(version).then(
-      (data) => {
-        if (ignored) {
-          return;
-        }
-
-        dispatch({ type: "success", payload: data });
-      },
-      () => {
-        if (!ignored) {
-          dispatch({ type: "failed" });
-        }
-      }
-    );
-
-    return () => {
-      ignored = true;
-    };
-  }, [version]);
+function SnapshotCardContents({ version }: { version: string }) {
+  const release = parseSnapshotRelease(version);
+  const { status, data: pullRequest } = useGitHubApi<GitHubPullRequest>(
+    `/repos/apollographql/apollo-client/pulls/${release?.prNumber}`,
+    { cache: true }
+  );
 
   if (status === "pending") {
     return (
@@ -97,13 +58,73 @@ function CardContents({ version }: { version: string }) {
     );
   }
 
-  const { release, latest } = data;
+  return (
+    <div className="flex flex-col gap-8">
+      <header className="flex flex-col gap-1 bg-primary dark:bg-primary-dark">
+        <h1 className="text-md text-heading dark:text-heading-dark font-heading font-medium flex items-center gap-2">
+          <IconGitHub className="size-4" />
+          <span className="whitespace-nowrap">{version}</span>
+          <Badge variant="info">Snapshot</Badge>
+        </h1>
+        {release && (
+          <>
+            <div className="flex gap-1 items-center text-xs font-bold uppercase text-secondary dark:text-secondary-dark">
+              Published{" "}
+              {formatPublishDate(parseSnapshotTimestamp(release.timestamp))}
+            </div>
+            <a
+              className="flex gap-1 items-center mt-2"
+              href={`https://github.com/apollographql/apollo-client/pull/${release.prNumber}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              View pull request in GitHub <IconOutlink className="size-3" />
+            </a>
+          </>
+        )}
+      </header>
 
-  const publishDate = new Intl.DateTimeFormat(undefined, {
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  }).format(Date.parse(release.published_at));
+      <section>
+        <h2 className="text-lg text-heading dark:text-heading-dark font-medium mb-2">
+          <Markdown>{pullRequest.title}</Markdown>
+        </h2>
+        <Markdown>{pullRequest.body}</Markdown>
+      </section>
+    </div>
+  );
+}
+
+function ReleaseCardContents({ version }: { version: string }) {
+  const currentRelease = useGitHubApi<GitHubRelease>(
+    `/repos/apollographql/apollo-client/releases/tags/v${version}`,
+    { cache: true }
+  );
+
+  const latestRelease = useGitHubApi<GitHubRelease>(
+    "/repos/apollographql/apollo-client/releases/latest",
+    { cache: true }
+  );
+
+  if (
+    currentRelease.status === "pending" ||
+    latestRelease.status === "pending"
+  ) {
+    return (
+      <div className="flex items-center justify-center min-w-80 min-h-80">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  if (currentRelease.status === "error" || latestRelease.status === "error") {
+    return (
+      <div className="flex min-w-80 min-h-80 items-center justify-center text-md font-semibold">
+        Error: Could not load release from GitHub
+      </div>
+    );
+  }
+
+  const release = currentRelease.data;
 
   return (
     <div className="flex flex-col gap-8">
@@ -113,14 +134,14 @@ function CardContents({ version }: { version: string }) {
           {release.name}{" "}
           {release.prerelease ? (
             <Badge variant="beta">Pre-release</Badge>
-          ) : latest ? (
+          ) : latestRelease.data.name === release.name ? (
             <Badge variant="success">Latest</Badge>
           ) : (
             <Badge variant="warning">Outdated</Badge>
           )}
         </h2>
         <div className="flex gap-1 items-center text-xs font-bold uppercase text-secondary dark:text-secondary-dark">
-          Published {publishDate}
+          Published {formatPublishDate(Date.parse(release.published_at))}
         </div>
         <a
           className="flex gap-1 items-center mt-2"
@@ -147,55 +168,29 @@ interface GitHubRelease {
   target_commitish: string;
 }
 
-interface GitHubError {
-  documentation_url: string;
-  message: string;
+interface GitHubPullRequest {
+  id: number;
+  number: number;
+  state: "open" | "closed";
+  locked: boolean;
+  title: string;
+  body: string;
+  created_at: string;
+  closed_at: string;
+  merged_at: string;
+  merge_commit_sha: string;
+  user: {
+    name: string;
+    login: string;
+    avatar_url: string;
+    url: string;
+  };
 }
 
-interface ReleaseData {
-  latest: boolean;
-  release: GitHubRelease;
-}
-
-const cache = new Map<string, ReleaseData>();
-
-async function getGitHubRelease(version: string): Promise<ReleaseData> {
-  if (cache.has(version)) {
-    return cache.get(version)!;
-  }
-
-  const headers = {
-    Accept: "application/vnd.github+json",
-    "X-GitHub-Api-Version": "2022-11-28",
-  };
-
-  async function parseResponse(response: Response) {
-    const body = await response.json();
-
-    if (response.ok) {
-      return body as GitHubRelease;
-    }
-
-    throw new Error((body as GitHubError).message);
-  }
-
-  const [release, latestRelease] = await Promise.all([
-    fetch(
-      `https://api.github.com/repos/apollographql/apollo-client/releases/tags/v${version}`,
-      { headers }
-    ).then(parseResponse),
-    fetch(
-      `https://api.github.com/repos/apollographql/apollo-client/releases/latest`,
-      { headers }
-    ).then(parseResponse),
-  ]);
-
-  const data = {
-    latest: release.name === latestRelease.name,
-    release,
-  };
-
-  cache.set(version, data);
-
-  return data;
+function formatPublishDate(date: Date | number) {
+  return new Intl.DateTimeFormat(undefined, {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  }).format(date);
 }
