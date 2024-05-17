bd34556750c684a4a92338767d41951de9552788
Jerel Miller <jerelmiller@gmail.com>
2024-05-16 22:08:16 -0600

    Fully implement StatusBadge

--- src/application/components/StatusBadge.tsx
+++ src/application/components/StatusBadge.tsx
@@ -1,25 +1,61 @@
 import type { ReactNode } from "react";
 import IconStatusDot from "@apollo/icons/default/IconStatusDot.svg";
+import type { VariantProps } from "class-variance-authority";
 import { cva } from "class-variance-authority";
+import type { OmitNull } from "../types/utils";
 
-interface StatusBadgeProps {
-  color: "green" | "red" | "purple";
+interface StatusBadgeProps extends IconVariants, ContainerVariants {
   children?: ReactNode;
 }
+
+type IconVariants = Required<OmitNull<VariantProps<typeof icon>>>;
+type ContainerVariants = Required<OmitNull<VariantProps<typeof container>>>;
+
+const container = cva(
+  [
+    "inline-flex",
+    "items-center",
+    "font-normal",
+    "font-normal",
+    "text-md",
+    "gap-2",
+  ],
+  {
+    variants: {
+      variant: {
+        rounded: [
+          "py-1",
+          "px-3",
+          "rounded-[100px]",
+          "border",
+          "border-primary",
+          "bg-primary",
+          "dark:border-primary-dark",
+          "dark:bg-primary-dark",
+        ],
+        hidden: [],
+      },
+    },
+  }
+);
 
 const icon = cva(["size-4"], {
   variants: {
     color: {
-      green: ["text-icon-success dark:text-icon-success-dark"],
-      red: ["text-icon-error dark:text-icon-error-dark"],
-      purple: ["text-icon-beta dark:text-icon-beta-dark"],
+      blue: ["text-icon-info", "text-icon-info-dark"],
+      gray: ["text-icon-secondary", "dark:text-icon-secondary-dark"],
+      green: ["text-icon-success", "dark:text-icon-success-dark"],
+      navy: ["text-icon-disabled", "dark:text-icon-disabled-dark"],
+      purple: ["text-icon-beta", "dark:text-icon-beta-dark"],
+      red: ["text-icon-error", "dark:text-icon-error-dark"],
+      yellow: ["text-icon-change", "dark:text-icon-change-dark"],
     },
   },
 });
 
-export function StatusBadge({ children, color }: StatusBadgeProps) {
+export function StatusBadge({ children, color, variant }: StatusBadgeProps) {
   return (
-    <span className="inline-flex items-center gap-1">
+    <span className={container({ variant })}>
       <IconStatusDot className={icon({ color })} />
       {children}
     </span>
