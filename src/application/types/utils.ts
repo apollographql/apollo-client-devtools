44fe6085863f1589042d00b368909d84e57bc46a
Jerel Miller <jerelmiller@gmail.com>
2024-05-16 23:08:33 -0600

    Create brand generic type

--- src/application/types/utils.ts
+++ src/application/types/utils.ts
@@ -3,3 +3,7 @@ export type OmitNull<T> = T extends object
       [K in keyof T]: Exclude<T[K], null>;
     }
   : Exclude<T, null>;
+
+declare const __brand: unique symbol;
+
+export type Brand<K, T> = K & { [__brand]: T };
