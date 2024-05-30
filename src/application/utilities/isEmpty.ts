74d94d1c5a1f977c8a053294379f395623f10f9a
Jerel Miller <jerelmiller@gmail.com>
2024-05-29 21:38:50 -0600

    Use general object in isEmpty

--- src/application/utilities/isEmpty.ts
+++ src/application/utilities/isEmpty.ts
@@ -1,5 +1,3 @@
-import type { JSONObject } from "../types/json";
-
-export function isEmpty(value: JSONObject | null | undefined) {
+export function isEmpty(value: object | null | undefined) {
   return value == null || Object.keys(value).length === 0;
 }
