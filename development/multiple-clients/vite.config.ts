0270137ca5acf4ed76c97d91917e9ccd8f71341b
Jerel Miller <jerelmiller@gmail.com>
2024-06-07 21:19:55 -0600

    Run multiple-clients app on port 3000

--- development/multiple-clients/vite.config.ts
+++ development/multiple-clients/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
   define: {
     "globalThis.__DEV__": JSON.stringify(true),
   },
