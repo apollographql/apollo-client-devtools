78b576a25a37350c34fd8ef8ff34090aee19149c
Jerel Miller <jerelmiller@gmail.com>
2024-06-13 17:38:48 -0600

    Remove watchedQuery field from local schema

--- src/application/types/resolvers.ts
+++ src/application/types/resolvers.ts
@@ -73,14 +73,9 @@ export type Query = {
   clientVersion?: Maybe<Scalars["String"]["output"]>;
   clients: Array<Client>;
   watchedQueries: WatchedQueries;
-  watchedQuery?: Maybe<WatchedQuery>;
 };
 
 export type QueryClientArgs = {
-  id: Scalars["ID"]["input"];
-};
-
-export type QueryWatchedQueryArgs = {
   id: Scalars["ID"]["input"];
 };
 
@@ -315,12 +310,6 @@ export type QueryResolvers<
     ResolversTypes["WatchedQueries"],
     ParentType,
     ContextType
-  >;
-  watchedQuery?: Resolver<
-    Maybe<ResolversTypes["WatchedQuery"]>,
-    ParentType,
-    ContextType,
-    RequireFields<QueryWatchedQueryArgs, "id">
   >;
 };
 
