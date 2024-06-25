2f8db62559d93e529fdee5c28aa15e5e2497f7fd
Jerel Miller <jerelmiller@gmail.com>
2024-06-24 22:14:24 -0600

    Ensure ListItem content is full width

--- src/application/components/ListItem.tsx
+++ src/application/components/ListItem.tsx
@@ -35,7 +35,7 @@ export function ListItem({
         }
       }}
     >
-      <div className="overflow-hidden whitespace-nowrap text-ellipsis">
+      <div className="overflow-hidden whitespace-nowrap text-ellipsis w-full">
         {children}
       </div>
     </li>
