bf14b90f8262daae8fb1a4e827465ea557386705
Jerel Miller <jerelmiller@gmail.com>
2024-07-07 23:02:38 -0600

    Add a timeout actor to devtools machine

--- src/application/machines.ts
+++ src/application/machines.ts
@@ -1,4 +1,5 @@
 import { createMachine } from "xstate";
+import { timeoutLogic } from "./actors/timeoutActor";
 
 type Events =
   | { type: "connect" }
@@ -17,47 +18,70 @@ export type StateValues =
 
 type Actions = { type: "connectToClient" } | { type: "notifyConnected" };
 
-export const devtoolsMachine = createMachine({
-  types: {
-    actions: {} as Actions,
-    events: {} as Events,
+export const devtoolsMachine = createMachine(
+  {
+    types: {
+      actions: {} as Actions,
+      events: {} as Events,
+    },
+    initial: "initialized",
+    states: {
+      initialized: {
+        on: {
+          connect: "connected",
+          timeout: "timedout",
+          clientNotFound: "notFound",
+        },
+        entry: ["connectToClient"],
+        invoke: {
+          id: "connectTimeout",
+          src: "timeout",
+          input: 10_000,
+          onDone: {
+            target: "notFound",
+          },
+        },
+      },
+      retrying: {
+        on: {
+          connect: "connected",
+          clientNotFound: "notFound",
+        },
+        entry: "connectToClient",
+      },
+      connected: {
+        on: {
+          disconnect: "disconnected",
+        },
+        entry: "notifyConnected",
+      },
+      disconnected: {
+        on: {
+          connect: "connected",
+          timeout: "timedout",
+          clientNotFound: "notFound",
+        },
+        invoke: {
+          id: "connectTimeout",
+          src: "timeout",
+          input: 10_000,
+          onDone: {
+            target: "notFound",
+          },
+        },
+      },
+      timedout: {},
+      notFound: {
+        on: {
+          retry: "retrying",
+          connect: "connected",
+        },
+      },
+    },
   },
-  initial: "initialized",
-  states: {
-    initialized: {
-      on: {
-        connect: "connected",
-        timeout: "timedout",
-        clientNotFound: "notFound",
-      },
-      entry: ["connectToClient"],
+  {
+    actors: {
+      timeout: timeoutLogic,
     },
-    retrying: {
-      on: {
-        connect: "connected",
-        clientNotFound: "notFound",
-      },
-      entry: "connectToClient",
-    },
-    connected: {
-      on: {
-        disconnect: "disconnected",
-      },
-      entry: "notifyConnected",
-    },
-    disconnected: {
-      on: {
-        connect: "connected",
-        timeout: "timedout",
-        clientNotFound: "notFound",
-      },
-    },
-    timedout: {},
-    notFound: {
-      on: {
-        retry: "retrying",
-        connect: "connected",
-      },
-    },
-  },
-});
+  }
+);
