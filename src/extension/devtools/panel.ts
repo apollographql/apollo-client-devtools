4021e1d10f62246e186b28d6156702da7910db61
Jerel Miller <jerelmiller@gmail.com>
2024-07-07 23:40:28 -0600

    Remove rest of commented code

--- src/extension/devtools/panel.ts
+++ src/extension/devtools/panel.ts
@@ -2,10 +2,4 @@ import { initDevTools } from "../../application";
 import "./panel.css";
 import { getPanelActor } from "./panelActor";
 
-const panelWindow = getPanelActor(window);
-
-panelWindow.on("initializePanel", initDevTools);
-
-// panelWindow.on("clientTerminated", (message) => {
-//   removeClient(message.clientId);
-// });
+getPanelActor(window).on("initializePanel", initDevTools);
