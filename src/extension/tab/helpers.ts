7e4be4e74130d25e83fcd26cd851bb0d48b9f659
Jerel Miller <jerelmiller@gmail.com>
2024-07-07 19:23:02 -0600

    Set default network status in legacy client

--- src/extension/tab/helpers.ts
+++ src/extension/tab/helpers.ts
@@ -156,7 +156,7 @@ export function getQueriesLegacy(
         document,
         variables,
         cachedData: diff?.result,
-        networkStatus,
+        networkStatus: networkStatus ?? 1,
       })
     );
   }
