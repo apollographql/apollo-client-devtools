156d9a04fe21e94d2caf3841993504f210c73b31
Jerel Miller <jerelmiller@gmail.com>
2024-07-08 18:27:36 -0600

    Use setup instead of createMachine

--- src/application/machines/devtoolsMachine.ts
+++ src/application/machines/devtoolsMachine.ts
@@ -1,4 +1,4 @@
-import { createMachine } from "xstate";
+import { setup } from "xstate";
 import { BannerAlert } from "../components/BannerAlert";
 
 type Events =
@@ -16,100 +16,96 @@ export type StateValues =
   | "timedout"
   | "notFound";
 
-type Actions =
-  | { type: "connectToClient" }
-  | { type: "closeBanner" }
-  | { type: "notifyConnected" }
-  | { type: "notifyDisconnected" }
-  | { type: "notifyNotFound" }
-  | { type: "notifyTimedOut" };
+function throwIfNotOverridden(name: string) {
+  return () => {
+    throw new Error(`Please override the '${name}' action with .provide`);
+  };
+}
 
-export const devtoolsMachine = createMachine(
-  {
-    types: {
-      actions: {} as Actions,
-      events: {} as Events,
+export const devtoolsMachine = setup({
+  types: {
+    events: {} as Events,
+  },
+  delays: {
+    connectToClientTimeout: 10_000,
+  },
+  actions: {
+    connectToClient: throwIfNotOverridden("connectToClient"),
+    closeBanner: BannerAlert.close,
+    notifyNotFound: throwIfNotOverridden("notifyNotFound"),
+    notifyDisconnected: () => {
+      BannerAlert.show({
+        type: "loading",
+        content: "Disconnected. Looking for client...",
+      });
     },
-    initial: "initialized",
-    states: {
-      initialized: {
-        on: {
-          connect: "connected",
-          timeout: "timedout",
-          clientNotFound: "notFound",
-        },
-        entry: "connectToClient",
-        after: {
-          connectToClientTimeout: {
-            target: "notFound",
-          },
-        },
-      },
-      retrying: {
-        on: {
-          connect: "connected",
-          clientNotFound: "notFound",
-        },
-        entry: "connectToClient",
-      },
-      connected: {
-        on: {
-          disconnect: "disconnected",
-        },
-        entry: "notifyConnected",
-        after: {
-          2500: {
-            actions: "closeBanner",
-          },
-        },
-      },
-      disconnected: {
-        on: {
-          connect: "connected",
-          timeout: "timedout",
-          clientNotFound: "notFound",
-        },
-        entry: "notifyDisconnected",
-        after: {
-          connectToClientTimeout: {
-            target: "notFound",
-          },
-        },
-      },
-      timedout: {
-        entry: "notifyTimedOut",
-      },
-      notFound: {
-        on: {
-          retry: "retrying",
-          connect: "connected",
-        },
-        entry: "notifyNotFound",
-      },
+    notifyConnected: () => {
+      BannerAlert.show({ type: "success", content: "Connected!" });
+    },
+    notifyTimedOut: () => {
+      BannerAlert.show({
+        type: "error",
+        content:
+          "Unable to communicate with browser tab. Please reload the window and restart the devtools to try again.",
+      });
     },
   },
-  {
-    delays: {
-      connectToClientTimeout: 10_000,
-    },
-    actions: {
-      closeBanner: BannerAlert.close,
-      notifyDisconnected: () => {
-        BannerAlert.show({
-          type: "loading",
-          content: "Disconnected. Looking for client...",
-        });
+}).createMachine({
+  initial: "initialized",
+  states: {
+    initialized: {
+      on: {
+        connect: "connected",
+        timeout: "timedout",
+        clientNotFound: "notFound",
       },
-      notifyConnected: () => {
-        BannerAlert.show({ type: "success", content: "Connected!" });
-      },
-      notifyTimedOut: () => {
-        BannerAlert.show({
-          type: "error",
-          content:
-            "Unable to communicate with browser tab. Please reload the window and restart the devtools to try again.",
-        });
+      entry: "connectToClient",
+      after: {
+        connectToClientTimeout: {
+          target: "notFound",
+        },
       },
     },
-  }
-);
+    retrying: {
+      on: {
+        connect: "connected",
+        clientNotFound: "notFound",
+      },
+      entry: "connectToClient",
+    },
+    connected: {
+      on: {
+        disconnect: "disconnected",
+      },
+      entry: "notifyConnected",
+      after: {
+        2500: {
+          actions: "closeBanner",
+        },
+      },
+    },
+    disconnected: {
+      on: {
+        connect: "connected",
+        timeout: "timedout",
+        clientNotFound: "notFound",
+      },
+      entry: "notifyDisconnected",
+      after: {
+        connectToClientTimeout: {
+          target: "notFound",
+        },
+      },
+    },
+    timedout: {
+      entry: "notifyTimedOut",
+    },
+    notFound: {
+      on: {
+        retry: "retrying",
+        connect: "connected",
+      },
+      entry: "notifyNotFound",
+    },
+  },
+});
