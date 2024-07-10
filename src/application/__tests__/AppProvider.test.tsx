9ad8e59e366b2587555fe341983261480ff98096
Jerel Miller <jerelmiller@gmail.com>
2024-07-09 23:55:05 -0600

    Remove outdated tests for helpers that no longer exist

--- src/application/__tests__/AppProvider.test.tsx
+++ src/application/__tests__/AppProvider.test.tsx
@@ -1,14 +1,8 @@
 import { render, waitFor, screen } from "@testing-library/react";
-import { NetworkStatus, gql } from "@apollo/client";
 
 import matchMediaMock from "../utilities/testing/matchMedia";
 import { Mode, colorTheme } from "../theme";
-import { AppProvider, getQueryData, getMutationData } from "../index";
-import type {
-  MutationDetails,
-  QueryDetails,
-} from "../../extension/tab/helpers";
-import { print, getIntrospectionQuery } from "graphql";
+import { AppProvider } from "../index";
 
 const matchMedia = matchMediaMock();
 
@@ -25,95 +19,6 @@ describe("<AppProvider />", () => {
     await waitFor(() => {
       matchMedia.useMediaQuery(Mode.Dark);
       expect(colorTheme()).toEqual("dark");
-    });
-  });
-
-  describe("getQueryData", () => {
-    test("returns expected query data", () => {
-      const queryData: QueryDetails = {
-        id: "1",
-        document: gql`
-          query GetColorByHex {
-            someQuery {
-              id
-              __typename
-            }
-          }
-        `,
-        variables: {
-          color: "#ee82ee",
-        },
-        cachedData: {
-          name: "Violet",
-        },
-        options: { fetchPolicy: "network-only" },
-        networkStatus: NetworkStatus.ready,
-      };
-
-      const data = getQueryData(queryData);
-
-      expect(data).toEqual({
-        id: 1,
-        __typename: "WatchedQuery",
-        name: "GetColorByHex",
-        queryString:
-          "query GetColorByHex {\n  someQuery {\n    id\n    __typename\n  }\n}",
-        variables: {
-          color: "#ee82ee",
-        },
-        cachedData: {
-          name: "Violet",
-        },
-        options: {
-          fetchPolicy: "network-only",
-        },
-        pollInterval: null,
-        error: null,
-        networkStatus: NetworkStatus.ready,
-      });
-    });
-
-    test("ignores IntrospectionQuery", () => {
-      const queryData: QueryDetails = {
-        id: "1",
-        document: gql(getIntrospectionQuery()),
-      };
-
-      const data = getQueryData(queryData);
-      expect(data).toBeUndefined();
-    });
-  });
-
-  describe("getMutationData", () => {
-    test("returns expected mutation data", () => {
-      const mutation = gql`
-        mutation SaveColor {
-          saveColor
-        }
-      `;
-
-      const mutationData: MutationDetails = {
-        document: mutation,
-        variables: {
-          color: "#ee82ee",
-        },
-        loading: false,
-        error: null,
-      };
-
-      const data = getMutationData(mutationData, 0);
-
-      expect(data).toEqual({
-        id: 0,
-        __typename: "WatchedMutation",
-        name: "SaveColor",
-        mutationString: print(mutation),
-        variables: {
-          color: "#ee82ee",
-        },
-        loading: false,
-        error: null,
-      });
     });
   });
 });
