bae0803edde0c67d2b3333b3474e8e55ebca31f4
Jerel Miller <jerelmiller@gmail.com>
2024-07-09 23:51:08 -0600

    Fix app test by mocking rpc call

--- src/application/__tests__/App.test.tsx
+++ src/application/__tests__/App.test.tsx
@@ -3,15 +3,30 @@ import { screen, waitFor } from "@testing-library/react";
 import { renderWithApolloClient } from "../utilities/testing/renderWithApolloClient";
 import { currentScreen, Screens } from "../components/Layouts/Navigation";
 import { App } from "../App";
+import { getRpcClient } from "../../extension/devtools/panelRpcClient";
+import type { GetRpcClientMock } from "../../extension/devtools/__mocks__/panelRpcClient";
+
+jest.mock("../../extension/devtools/panelRpcClient.ts");
 
 jest.mock("../components/Explorer/Explorer", () => ({
   Explorer: () => <div>Build</div>,
 }));
 
+const testAdapter = (getRpcClient as GetRpcClientMock).__adapter;
+
+beforeEach(() => {
+  testAdapter.mockClear();
+});
+
 describe("<App />", () => {
   test("renders the selected screen", async () => {
+    testAdapter.handleRpcRequest("getClients", () => []);
+
     renderWithApolloClient(<App />);
-    expect(screen.getByText("Queries (0)")).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.getByText("Queries (0)")).toBeInTheDocument();
+    });
     await waitFor(() => {
       currentScreen(Screens.Mutations);
       expect(currentScreen()).toEqual(Screens.Mutations);
