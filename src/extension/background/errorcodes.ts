fc3a211d8c4fc0a491d27400ba952ebca9f564a8
Jerel Miller <jerelmiller@gmail.com>
2024-07-25 23:39:28 -0600

    Remove ErrorCodesHandler type

--- src/extension/background/errorcodes.ts
+++ src/extension/background/errorcodes.ts
@@ -1,13 +1,9 @@
 import browser from "webextension-polyfill";
 import { createRpcHandler } from "../rpc";
 import { createPortMessageAdapter } from "../messageAdapters";
-import type { ErrorCodes } from "@apollo/client/invariantErrorCodes.js";
 import allErrorCodes from "../../../all-clients/errorcodes.json";
 import { restoreErrorCodes } from "../../../all-clients/restore-errorcodes.mjs";
 
-export type ErrorCodesHandler = {
-  getErrorCodes(version: string): Promise<ErrorCodes | undefined>;
-};
 browser.runtime.onConnect.addListener((port) => {
   if (port.name === "tab") {
     const handleRpc = createRpcHandler(createPortMessageAdapter(() => port));
