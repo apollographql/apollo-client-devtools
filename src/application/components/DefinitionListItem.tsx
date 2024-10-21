0fd9654affb6f5a850b2292a22f1f70c4fd630e5
Jerel Miller <jerelmiller@gmail.com>
2024-10-21 12:48:25 -0600

    Use children as value in DefinitionListItem

--- src/application/components/DefinitionListItem.tsx
+++ src/application/components/DefinitionListItem.tsx
@@ -1,22 +1,20 @@
-import type { ComponentPropsWithoutRef, ReactNode } from "react";
+import type { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface DefinitionListItemProps
-  extends Omit<ComponentPropsWithoutRef<"div">, "children"> {
+interface DefinitionListItemProps extends ComponentPropsWithoutRef<"div"> {
   term: string;
-  value: ReactNode;
 }
 
 export function DefinitionListItem({
+  children,
   className,
   term,
-  value,
   ...props
 }: DefinitionListItemProps) {
   return (
     <div {...props} className={twMerge("flex gap-3 py-2", className)}>
       <dt className="font-semibold">{term}</dt>
-      <dd>{value}</dd>
+      <dd>{children}</dd>
     </div>
   );
 }
