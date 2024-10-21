578806de23a57c0b470f0b08ef5998b864d87981
Jerel Miller <jerelmiller@gmail.com>
2024-10-21 14:28:29 -0600

    Update to use empty state and body components

--- src/application/pages/connectors/$operationId/requests/$requestId.tsx
+++ src/application/pages/connectors/$operationId/requests/$requestId.tsx
@@ -2,10 +2,11 @@ import { useParams } from "react-router-dom";
 import { useRequest } from "../../$operationId";
 import { useMemo } from "react";
 import { Tabs } from "../../../../components/Tabs";
-import { JSONTreeViewer } from "../../../../components/JSONTreeViewer";
 import IconStatusDot from "@apollo/icons/default/IconStatusDot.svg";
 import { ConnectorsRequestOverview } from "../../../../components/ConnectorsRequestOverview";
 import { ConnectorsResponseOverview } from "../../../../components/ConnectorsResponseOverview";
+import { ConnectorsBody } from "../../../../components/ConnectorsBody";
+import { ConnectorsEmptyState } from "../../../../components/ConnectorsEmptyState";
 
 export function Route() {
   const params = useParams();
@@ -26,10 +27,11 @@ export function Route() {
     <Tabs defaultValue="request">
       <Tabs.List>
         <Tabs.Trigger value="request">Request overview</Tabs.Trigger>
-        <Tabs.Trigger value="response">Response overview</Tabs.Trigger>
-        {response?.body && (
-          <Tabs.Trigger value="responseBody">Response body</Tabs.Trigger>
+        {!!data.request?.body && (
+          <Tabs.Trigger value="requestBody">Request body</Tabs.Trigger>
         )}
+        <Tabs.Trigger value="response">Response overview</Tabs.Trigger>
+        <Tabs.Trigger value="responseBody">Response body</Tabs.Trigger>
         <Tabs.Trigger value="mapping" className="flex items-center gap-2">
           Mapping
           {selectionErrorCount > 0 && (
@@ -41,30 +43,30 @@ export function Route() {
         {data.request ? (
           <ConnectorsRequestOverview request={data.request} />
         ) : (
-          "Empty state here"
+          <ConnectorsEmptyState>
+            No request details to show
+          </ConnectorsEmptyState>
         )}
+      </Tabs.Content>
+      <Tabs.Content value="requestBody" className="py-4">
+        {data.request?.body && <ConnectorsBody body={data.request.body} />}
       </Tabs.Content>
       <Tabs.Content value="response" className="py-4">
         {data.response ? (
           <ConnectorsResponseOverview response={data.response} />
         ) : (
-          "Empty state here"
+          <ConnectorsEmptyState>
+            No response details to show
+          </ConnectorsEmptyState>
         )}
       </Tabs.Content>
-      {response?.body && (
-        <Tabs.Content value="responseBody" className="py-4">
-          {response.body.kind === "json" ? (
-            <JSONTreeViewer
-              hideRoot={!Array.isArray(response.body.content)}
-              className="[&>li]:!pt-0"
-              data={response.body.content}
-              shouldExpandNodeInitially={() => true}
-            />
-          ) : (
-            String(response.body.content)
-          )}
-        </Tabs.Content>
-      )}
+      <Tabs.Content value="responseBody" className="py-4">
+        {response?.body ? (
+          <ConnectorsBody body={response.body} />
+        ) : (
+          <ConnectorsEmptyState>No response body to show</ConnectorsEmptyState>
+        )}
+      </Tabs.Content>
       <Tabs.Content value="mapping" className="py-4">
         <div>Mapping: </div>
       </Tabs.Content>
