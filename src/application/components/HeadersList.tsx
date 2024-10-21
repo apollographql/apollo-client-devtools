adcfc0e45a7654387c37f7b1c0865d6c0b983a66
Jerel Miller <jerelmiller@gmail.com>
2024-10-21 12:47:26 -0600

    Create a HeadersList component

---
+++ src/application/components/HeadersList.tsx
@@ -0,0 +1,16 @@
+import { DefinitionList } from "./DefinitionList";
+import { DefinitionListItem } from "./DefinitionListItem";
+
+interface HeadersListProps {
+  headers: Array<[string, string]>;
+}
+
+export function HeadersList({ headers }: HeadersListProps) {
+  return (
+    <DefinitionList>
+      {headers.map(([name, value], idx) => (
+        <DefinitionListItem key={idx} term={name} value={value} />
+      ))}
+    </DefinitionList>
+  );
+}
