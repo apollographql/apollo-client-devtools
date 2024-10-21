b3e29d0c98375e82b07cc981a24b93b8abf47b31
Jerel Miller <jerelmiller@gmail.com>
2024-10-21 16:06:09 -0600

    Ensure Tr uses forwardRef

--- src/application/components/Tr.tsx
+++ src/application/components/Tr.tsx
@@ -1,16 +1,19 @@
-import type { ComponentPropsWithoutRef } from "react";
+import { forwardRef, type ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 type TrProps = ComponentPropsWithoutRef<"tr">;
 
-export function Tr({ className, ...props }: TrProps) {
-  return (
-    <tr
-      {...props}
-      className={twMerge(
-        "focus-visible:outline-0 focus-visible:bg-tableHover dark:focus-visible:bg-tableHover-dark group/tr",
-        className
-      )}
-    />
-  );
-}
+export const Tr = forwardRef<HTMLTableRowElement, TrProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <tr
+        {...props}
+        ref={ref}
+        className={twMerge(
+          "focus-visible:outline-0 focus-visible:bg-tableHover dark:focus-visible:bg-tableHover-dark group/tr",
+          className
+        )}
+      />
+    );
+  }
+);
