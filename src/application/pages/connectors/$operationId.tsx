4f17196ab0a815db170763756fc8acc3504e67ed
Jerel Miller <jerelmiller@gmail.com>
2024-10-21 19:16:03 -0600

    Combine UI

--- src/application/pages/connectors/$operationId.tsx
+++ src/application/pages/connectors/$operationId.tsx
@@ -30,24 +30,8 @@ export function Route() {
   const { request } = useLoaderData() as ReturnType<typeof loader>;
   const match = useMatch("/connectors/:operationId/requests/:requestId");
 
-  if (!request) {
-    return (
-      <SidebarLayout.Main className="!overflow-auto flex flex-col p-4 gap-4">
-        <Heading as="h1" size="2xl">
-          Connectors
-        </Heading>
-        <Breadcrumb>
-          <BreadcrumbItem isCurrentPage>
-            <BreadcrumbLink to="#">All requests</BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
-        <Alert variant="error">Connectors request not found</Alert>
-      </SidebarLayout.Main>
-    );
-  }
-
   const selectedRequest = match
-    ? request.debuggingResult.data.find(
+    ? request?.debuggingResult.data.find(
         (data) => String(data.id) === match.params.requestId
       )
     : null;
@@ -59,43 +43,53 @@ export function Route() {
   return (
     <>
       <SidebarLayout.Main className="!overflow-auto flex flex-col p-4 gap-4">
-        <Heading as="h1" size="2xl">
-          Connectors
-        </Heading>
-        <Breadcrumb>
-          <BreadcrumbItem isCurrentPage={!match}>
-            <BreadcrumbLink to={`/connectors/${request.id}`}>
-              All requests
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          {selectedURL && (
-            <BreadcrumbItem isCurrentPage>
-              <BreadcrumbLink to="#">{selectedURL?.pathname}</BreadcrumbLink>
+        <div className="flex flex-col gap-2">
+          <Heading as="h1" size="2xl">
+            Connectors
+          </Heading>
+          <Breadcrumb>
+            <BreadcrumbItem isCurrentPage={!match}>
+              <BreadcrumbLink to={`/connectors/${request?.id}`}>
+                All requests
+              </BreadcrumbLink>
             </BreadcrumbItem>
-          )}
-        </Breadcrumb>
-        <Outlet context={request} />
+            {selectedURL && (
+              <BreadcrumbItem isCurrentPage>
+                <BreadcrumbLink to="#">{selectedURL?.pathname}</BreadcrumbLink>
+              </BreadcrumbItem>
+            )}
+          </Breadcrumb>
+        </div>
+        {request ? (
+          <Outlet context={request} />
+        ) : (
+          <Alert variant="error">Connectors request not found</Alert>
+        )}
       </SidebarLayout.Main>
-      <PanelResizeHandle className="border-r border-primary dark:border-primary-dark" />
-      <Panel
-        id="details"
-        defaultSize={25}
-        minSize={25}
-        className="h-full p-4 flex flex-col gap-2"
-      >
-        <h2 className="text-heading dark:text-heading-dark font-medium text-lg">
-          Query
-        </h2>
-        <CodeBlock language="graphql" code={request.query} />
-        <h2 className="text-heading dark:text-heading-dark font-medium text-lg">
-          Variables
-        </h2>
-        <JSONTreeViewer
-          hideRoot={!isEmpty(request.variables)}
-          className="[&>li]:!pt-0"
-          data={request.variables ?? {}}
-        />
-      </Panel>
+      {request && (
+        <>
+          <PanelResizeHandle className="border-r border-primary dark:border-primary-dark" />
+          <Panel
+            id="details"
+            defaultSize={25}
+            minSize={25}
+            className="h-full p-4 flex flex-col gap-2"
+          >
+            <h2 className="text-heading dark:text-heading-dark font-medium text-lg">
+              Query
+            </h2>
+            <CodeBlock language="graphql" code={request.query} />
+            <h2 className="text-heading dark:text-heading-dark font-medium text-lg">
+              Variables
+            </h2>
+            <JSONTreeViewer
+              hideRoot={!isEmpty(request.variables)}
+              className="[&>li]:!pt-0"
+              data={request.variables ?? {}}
+            />
+          </Panel>
+        </>
+      )}
     </>
   );
 }
