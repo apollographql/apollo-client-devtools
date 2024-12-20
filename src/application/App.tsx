fcd5b93ee6fc88d870937e4e168f5e8e1cd79ecd
Jerel Miller <jerelmiller@gmail.com>
2024-12-20 15:53:27 -0700

    Remove unneeded forceMount

--- src/application/App.tsx
+++ src/application/App.tsx
@@ -275,7 +275,6 @@ export const App = () => {
         <Tabs.Content
           className="flex flex-1 overflow-hidden"
           value={Screens.Memory}
-          forceMount
         >
           <TabErrorBoundary remarks="Error on Memory tab:">
             <MemoryInternals clientId={selectedClientId} />
