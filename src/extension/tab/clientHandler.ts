f74d3aa4278f7c04f22bbe4f8cb619b354252f53
Jerel Miller <jerelmiller@gmail.com>
2025-08-23 19:17:20 -0600

    Better type management for internal/external client access

--- src/extension/tab/clientHandler.ts
+++ src/extension/tab/clientHandler.ts
@@ -9,11 +9,19 @@ export abstract class ClientHandler<
   TClient extends ApolloClient3<any> | ApolloClient4,
 > {
   readonly id: string;
-  readonly client: WithPrivateAccess<TClient>;
+  protected readonly client: WithPrivateAccess<TClient>;
 
   constructor(client: TClient) {
     this.id = createId();
     this.client = getPrivateAccess(client);
+  }
+
+  get version() {
+    return this.client.version;
+  }
+
+  getClient() {
+    return this.client as unknown as TClient;
   }
 
   abstract getMutations(): TClient extends ApolloClient3<any>
