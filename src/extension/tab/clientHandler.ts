d8639eb46bd7c95e0c9302b6d768a401a312bb10
Jerel Miller <jerelmiller@gmail.com>
2025-08-23 18:22:09 -0600

    Get private access by default in handler

--- src/extension/tab/clientHandler.ts
+++ src/extension/tab/clientHandler.ts
@@ -1,14 +1,16 @@
 import type { ApolloClient as ApolloClient4 } from "@apollo/client";
 import type { ApolloClient as ApolloClient3 } from "@apollo/client-3";
 import type { MutationDetails } from "./v3/types";
+import type { WithPrivateAccess } from "../../privateAccess";
+import { getPrivateAccess } from "../../privateAccess";
 
 export abstract class ClientHandler<
   TClient extends ApolloClient3<any> | ApolloClient4,
 > {
-  client: TClient;
+  client: WithPrivateAccess<TClient>;
 
   constructor(client: TClient) {
-    this.client = client;
+    this.client = getPrivateAccess(client);
   }
 
   abstract getMutations(): TClient extends ApolloClient3<any>
