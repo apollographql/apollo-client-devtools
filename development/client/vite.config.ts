6c33b334e56243d57aa8fe63d0675fee08eaf268
Jerel Miller <jerelmiller@gmail.com>
2025-08-29 13:22:20 -0600

    Remove vite config option

--- development/client/vite.config.ts
+++ development/client/vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  define: {
-    "globalThis.__DEV__": JSON.stringify(true),
-  },
   plugins: [react()],
   server: {
     open: true,
