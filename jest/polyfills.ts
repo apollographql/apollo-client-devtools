681e411993020a3c7ae456bd0902ae6ecf20792d
Jerel Miller <jerelmiller@gmail.com>
2025-10-28 21:40:40 -0600

    Polyfill requestIdleCallback

--- jest/polyfills.ts
+++ jest/polyfills.ts
@@ -2,4 +2,15 @@ import { ReadableStream } from "node:stream/web";
 
 Object.defineProperties(globalThis, {
   ReadableStream: { value: ReadableStream },
+  requestIdleCallback: {
+    value: ((cb, options) => {
+      const id = setTimeout(() => {
+        cb({ didTimeout: false, timeRemaining: () => 0 });
+      }, options?.timeout);
+      return id as unknown as number;
+    }) as typeof requestIdleCallback,
+  },
+  cancelIdleCallback: {
+    value: (id: number) => clearTimeout(id),
+  },
 });
