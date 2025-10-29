99eecd5263a1b50d549f3eaf20cc558a3781d543
Jerel Miller <jerelmiller@gmail.com>
2025-10-28 21:53:20 -0600

    Send cache contents before/after write

--- src/extension/tab/shared/types.ts
+++ src/extension/tab/shared/types.ts
@@ -1,4 +1,5 @@
 import type { JSONObject } from "@/application/types/json";
+import type { Cache } from "@/application/types/scalars";
 import type { DocumentNode, OperationVariables } from "@apollo/client";
 
 export interface CacheWrite {
@@ -8,4 +9,8 @@ export interface CacheWrite {
   dataId: string | undefined;
   overwrite: boolean | undefined;
   broadcast: boolean | undefined;
+  cache: {
+    before: Cache;
+    after: Cache;
+  };
 }
