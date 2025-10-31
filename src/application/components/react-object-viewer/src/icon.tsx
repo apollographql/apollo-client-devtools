98b0e3b37245ce58876de7b0e390c1021b08f822
Jerel Miller <jerelmiller@gmail.com>
2025-10-31 13:50:08 -0600

    Format icons

--- src/application/components/react-object-viewer/src/icon.tsx
+++ src/application/components/react-object-viewer/src/icon.tsx
@@ -1,97 +1,211 @@
-export function IconArrow () {
+export function IconArrow() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="5" height="7" viewBox="0 0 5 7" className="icon-arrow">
-      <path d="M4.64 2.732l-3-2.5A1 1 0 0 0 0 1v5a1 1 0 0 0 1.64.768l3-2.5a1 1 0 0 0 0-1.536z" fillRule="evenodd" opacity=".675"/>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="5"
+      height="7"
+      viewBox="0 0 5 7"
+      className="icon-arrow"
+    >
+      <path
+        d="M4.64 2.732l-3-2.5A1 1 0 0 0 0 1v5a1 1 0 0 0 1.64.768l3-2.5a1 1 0 0 0 0-1.536z"
+        fillRule="evenodd"
+        opacity=".675"
+      />
     </svg>
-  )
+  );
 }
 
-export function IconEllipsis () {
+export function IconEllipsis() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="icon-ellipsis" fill="currentColor">
-      <path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"/>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 448 512"
+      className="icon-ellipsis"
+      fill="currentColor"
+    >
+      <path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z" />
     </svg>
-  )
+  );
 }
 
-export function IconObject () {
+export function IconObject() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 16 16">
-      <path d="M16 12a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4" className="icon-object"></path>
-      <path d="M13.1 7.31l-1.15-.5V5.3A2.21 2.21 0 0 0 9.6 3.15a.75.75 0 0 0 0 1.5c.26 0 .86.08.86.65v2a.75.75 0 0 0 .44.7.75.75 0 0 0-.45.69v2c0 .3-.17.66-.85.66a.75.75 0 1 0 0 1.5 2.18 2.18 0 0 0 2.35-2.15V9.19l1.15-.5a.75.75 0 0 0 0-1.37zM5.55 7.3v-2c0-.57.6-.65.86-.65a.75.75 0 0 0 0-1.5A2.21 2.21 0 0 0 4.05 5.3v1.51l-1.15.5a.75.75 0 0 0 0 1.38l1.15.5v1.51a2.18 2.18 0 0 0 2.35 2.15.75.75 0 0 0 0-1.5c-.68 0-.84-.36-.85-.65v-2A.75.75 0 0 0 5.1 8a.75.75 0 0 0 .45-.7z" className="icon-inner"></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="M16 12a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4"
+        className="icon-object"
+      ></path>
+      <path
+        d="M13.1 7.31l-1.15-.5V5.3A2.21 2.21 0 0 0 9.6 3.15a.75.75 0 0 0 0 1.5c.26 0 .86.08.86.65v2a.75.75 0 0 0 .44.7.75.75 0 0 0-.45.69v2c0 .3-.17.66-.85.66a.75.75 0 1 0 0 1.5 2.18 2.18 0 0 0 2.35-2.15V9.19l1.15-.5a.75.75 0 0 0 0-1.37zM5.55 7.3v-2c0-.57.6-.65.86-.65a.75.75 0 0 0 0-1.5A2.21 2.21 0 0 0 4.05 5.3v1.51l-1.15.5a.75.75 0 0 0 0 1.38l1.15.5v1.51a2.18 2.18 0 0 0 2.35 2.15.75.75 0 0 0 0-1.5c-.68 0-.84-.36-.85-.65v-2A.75.75 0 0 0 5.1 8a.75.75 0 0 0 .45-.7z"
+        className="icon-inner"
+      ></path>
     </svg>
-  )
+  );
 }
 
-export function IconArray () {
+export function IconArray() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 16 16">
-      <path d="M12 16H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4z" className="icon-array"></path>
-      <path d="M6.7 12.65h-2a.75.75 0 0 1-.75-.75v-7a.75.75 0 0 1 .75-.75h2a.75.75 0 0 1 0 1.5H5.45v5.5H6.7a.75.75 0 1 1 0 1.5zM11.7 12.65h-2a.75.75 0 0 1 0-1.5h1.25v-5.5H9.7a.75.75 0 0 1 0-1.5h2a.75.75 0 0 1 .75.75v7a.75.75 0 0 1-.75.75z" className="icon-inner"></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="M12 16H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4z"
+        className="icon-array"
+      ></path>
+      <path
+        d="M6.7 12.65h-2a.75.75 0 0 1-.75-.75v-7a.75.75 0 0 1 .75-.75h2a.75.75 0 0 1 0 1.5H5.45v5.5H6.7a.75.75 0 1 1 0 1.5zM11.7 12.65h-2a.75.75 0 0 1 0-1.5h1.25v-5.5H9.7a.75.75 0 0 1 0-1.5h2a.75.75 0 0 1 .75.75v7a.75.75 0 0 1-.75.75z"
+        className="icon-inner"
+      ></path>
     </svg>
-  )
+  );
 }
 
-export function IconString () {
+export function IconString() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 16 16">
-      <path d="M16 12a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4" className="icon-string"></path>
-      <path d="M6.4 5.65h.2a.75.75 0 0 0 0-1.5h-.2A2.55 2.55 0 0 0 3.85 6.7v4.2a.75.75 0 0 0 .75.75.74.74 0 0 0 .25-.05v.05H6.1a1.2 1.2 0 0 0 1.25-1.25A1.2 1.2 0 0 0 6.1 9.15h-.75V6.7A1 1 0 0 1 6.4 5.65zM11.1 9.15h-.75V6.3a.61.61 0 0 1 .65-.65h.6a.75.75 0 0 0 0-1.5H11A2.13 2.13 0 0 0 8.85 6.3v4.6a.75.75 0 0 0 .75.75.74.74 0 0 0 .25-.05v.05h1.25a1.2 1.2 0 0 0 1.25-1.25 1.2 1.2 0 0 0-1.25-1.25z" className="icon-inner"></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="M16 12a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4"
+        className="icon-string"
+      ></path>
+      <path
+        d="M6.4 5.65h.2a.75.75 0 0 0 0-1.5h-.2A2.55 2.55 0 0 0 3.85 6.7v4.2a.75.75 0 0 0 .75.75.74.74 0 0 0 .25-.05v.05H6.1a1.2 1.2 0 0 0 1.25-1.25A1.2 1.2 0 0 0 6.1 9.15h-.75V6.7A1 1 0 0 1 6.4 5.65zM11.1 9.15h-.75V6.3a.61.61 0 0 1 .65-.65h.6a.75.75 0 0 0 0-1.5H11A2.13 2.13 0 0 0 8.85 6.3v4.6a.75.75 0 0 0 .75.75.74.74 0 0 0 .25-.05v.05h1.25a1.2 1.2 0 0 0 1.25-1.25 1.2 1.2 0 0 0-1.25-1.25z"
+        className="icon-inner"
+      ></path>
     </svg>
-  )
+  );
 }
 
-export function IconNumber () {
+export function IconNumber() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 16 16">
-      <path d="M12 16H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4z" className="icon-number"></path>
-      <path d="M12 6h-1l.26-1a.75.75 0 1 0-1.45-.38L9.41 6H7.87l.26-1a.75.75 0 1 0-1.45-.37L6.32 6H5a.75.75 0 0 0 0 1.5h.9l-.42 1.75H4a.75.75 0 0 0 0 1.5h1.1l-.22.86a.75.75 0 0 0 .54.91h.19a.75.75 0 0 0 .72-.52l.32-1.24h1.52l-.2.76a.75.75 0 0 0 .54.92h.19a.75.75 0 0 0 .72-.56l.3-1.14H11a.75.75 0 0 0 0-1.5h-.89l.46-1.75H12A.75.75 0 0 0 12 6zM8.56 9.25H7l.48-1.75H9z" className="icon-inner"></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="M12 16H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4z"
+        className="icon-number"
+      ></path>
+      <path
+        d="M12 6h-1l.26-1a.75.75 0 1 0-1.45-.38L9.41 6H7.87l.26-1a.75.75 0 1 0-1.45-.37L6.32 6H5a.75.75 0 0 0 0 1.5h.9l-.42 1.75H4a.75.75 0 0 0 0 1.5h1.1l-.22.86a.75.75 0 0 0 .54.91h.19a.75.75 0 0 0 .72-.52l.32-1.24h1.52l-.2.76a.75.75 0 0 0 .54.92h.19a.75.75 0 0 0 .72-.56l.3-1.14H11a.75.75 0 0 0 0-1.5h-.89l.46-1.75H12A.75.75 0 0 0 12 6zM8.56 9.25H7l.48-1.75H9z"
+        className="icon-inner"
+      ></path>
     </svg>
-  )
+  );
 }
 
-export function IconBoolean () {
+export function IconBoolean() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 16.05 16.05">
-      <path d="M12 16.05L4 16a4 4 0 0 1-4-4l.05-8a4 4 0 0 1 4-4l8 .05a4 4 0 0 1 4 4l-.05 8a4 4 0 0 1-4 4z" className="icon-boolean"></path>
-      <path d="M7.53 4.28h-4a.75.75 0 0 0 0 1.5h1.24v5.25a.75.75 0 0 0 .75.75.75.75 0 0 0 .72-.78V5.75h1.29a.75.75 0 0 0 0-1.5zM12.51 7.32h-2.25V5.8h2.25a.75.75 0 0 0 0-1.5h-3a.75.75 0 0 0-.73.7v6a.75.75 0 0 0 .75.75.75.75 0 0 0 .75-.75V8.8h2.25a.75.75 0 0 0 0-1.5z" className="icon-inner"></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 16.05 16.05"
+    >
+      <path
+        d="M12 16.05L4 16a4 4 0 0 1-4-4l.05-8a4 4 0 0 1 4-4l8 .05a4 4 0 0 1 4 4l-.05 8a4 4 0 0 1-4 4z"
+        className="icon-boolean"
+      ></path>
+      <path
+        d="M7.53 4.28h-4a.75.75 0 0 0 0 1.5h1.24v5.25a.75.75 0 0 0 .75.75.75.75 0 0 0 .72-.78V5.75h1.29a.75.75 0 0 0 0-1.5zM12.51 7.32h-2.25V5.8h2.25a.75.75 0 0 0 0-1.5h-3a.75.75 0 0 0-.73.7v6a.75.75 0 0 0 .75.75.75.75 0 0 0 .75-.75V8.8h2.25a.75.75 0 0 0 0-1.5z"
+        className="icon-inner"
+      ></path>
     </svg>
-  )
+  );
 }
 
-export function IconNull () {
+export function IconNull() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 16 16">
-      <path d="M16 12.3a3.76 3.76 0 0 1-3.7 3.7H3.7A3.69 3.69 0 0 1 0 12.3V3.7A3.69 3.69 0 0 1 3.7 0h8.6A3.69 3.69 0 0 1 16 3.7z" className="icon-null"></path>
-      <path d="M10.68 4.73l.48-.86a.75.75 0 1 0-1.31-.73L9.37 4A4.2 4.2 0 0 0 8 3.75a4.23 4.23 0 0 0-2.68 7.52l-.48.86a.75.75 0 1 0 1.31.73l.48-.86a4.2 4.2 0 0 0 1.37.25 4.23 4.23 0 0 0 2.68-7.52zM5.25 8a2.73 2.73 0 0 1 3.38-2.67L6.06 9.95A2.74 2.74 0 0 1 5.25 8zM8 10.75a2.73 2.73 0 0 1-.63-.08l2.57-4.62A2.75 2.75 0 0 1 8 10.75z" className="icon-inner"></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="M16 12.3a3.76 3.76 0 0 1-3.7 3.7H3.7A3.69 3.69 0 0 1 0 12.3V3.7A3.69 3.69 0 0 1 3.7 0h8.6A3.69 3.69 0 0 1 16 3.7z"
+        className="icon-null"
+      ></path>
+      <path
+        d="M10.68 4.73l.48-.86a.75.75 0 1 0-1.31-.73L9.37 4A4.2 4.2 0 0 0 8 3.75a4.23 4.23 0 0 0-2.68 7.52l-.48.86a.75.75 0 1 0 1.31.73l.48-.86a4.2 4.2 0 0 0 1.37.25 4.23 4.23 0 0 0 2.68-7.52zM5.25 8a2.73 2.73 0 0 1 3.38-2.67L6.06 9.95A2.74 2.74 0 0 1 5.25 8zM8 10.75a2.73 2.73 0 0 1-.63-.08l2.57-4.62A2.75 2.75 0 0 1 8 10.75z"
+        className="icon-inner"
+      ></path>
     </svg>
-  )
+  );
 }
 
-export function IconUndefined () {
+export function IconUndefined() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 16 16">
-      <path d="M12.3 16H3.7A3.69 3.69 0 0 1 0 12.3V3.7A3.69 3.69 0 0 1 3.7 0h8.6A3.69 3.69 0 0 1 16 3.7v8.6a3.63 3.63 0 0 1-3.7 3.7z" className="icon-undefined"></path>
-      <path d="M7.9 10.35a.75.75 0 0 1-.75-.75V8.3a.75.75 0 0 1 .57-.73c.07 0 1.83-.49 1.83-2 0-.5-.27-1.25-1.75-1.25a1.39 1.39 0 0 0-1.31.75.76.76 0 0 1-1 .38.74.74 0 0 1-.4-1 2.88 2.88 0 0 1 2.71-1.6c1.9 0 3.12 1 3.25 2.69a3.62 3.62 0 0 1-2.4 3.29v.77a.75.75 0 0 1-.75.75z" className="icon-inner"></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="M12.3 16H3.7A3.69 3.69 0 0 1 0 12.3V3.7A3.69 3.69 0 0 1 3.7 0h8.6A3.69 3.69 0 0 1 16 3.7v8.6a3.63 3.63 0 0 1-3.7 3.7z"
+        className="icon-undefined"
+      ></path>
+      <path
+        d="M7.9 10.35a.75.75 0 0 1-.75-.75V8.3a.75.75 0 0 1 .57-.73c.07 0 1.83-.49 1.83-2 0-.5-.27-1.25-1.75-1.25a1.39 1.39 0 0 0-1.31.75.76.76 0 0 1-1 .38.74.74 0 0 1-.4-1 2.88 2.88 0 0 1 2.71-1.6c1.9 0 3.12 1 3.25 2.69a3.62 3.62 0 0 1-2.4 3.29v.77a.75.75 0 0 1-.75.75z"
+        className="icon-inner"
+      ></path>
       <circle cx="7.9" cy="12" r=".9" className="icon-inner"></circle>
     </svg>
-  )
+  );
 }
 
-export function IconSymbol () {
+export function IconSymbol() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 16 16">
-      <path d="M12 16H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4z" className="icon-symbol"></path>
-      <path d="M8.3 4l1.1 2.2c.1.1.2.2.3.2l2.5.4a.35.35 0 0 1 .2.6l-1.8 1.7a.37.37 0 0 0-.1.3l.4 2.5c.1.3-.3.5-.5.4l-2.2-1.2a.19.19 0 0 0-.3 0l-2.2 1.2c-.3.1-.6-.1-.5-.4l.4-2.5a.37.37 0 0 0-.1-.3L3.7 7.4a.35.35 0 0 1 .2-.6l2.5-.4c.1 0 .2-.1.3-.2L7.8 4c0-.3.4-.3.5 0z" className="icon-inner"></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="M12 16H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4z"
+        className="icon-symbol"
+      ></path>
+      <path
+        d="M8.3 4l1.1 2.2c.1.1.2.2.3.2l2.5.4a.35.35 0 0 1 .2.6l-1.8 1.7a.37.37 0 0 0-.1.3l.4 2.5c.1.3-.3.5-.5.4l-2.2-1.2a.19.19 0 0 0-.3 0l-2.2 1.2c-.3.1-.6-.1-.5-.4l.4-2.5a.37.37 0 0 0-.1-.3L3.7 7.4a.35.35 0 0 1 .2-.6l2.5-.4c.1 0 .2-.1.3-.2L7.8 4c0-.3.4-.3.5 0z"
+        className="icon-inner"
+      ></path>
     </svg>
-  )
+  );
 }
 
-export function IconFunction () {
+export function IconFunction() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 16 16">
-      <path d="M12 16H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4z" className="icon-function"></path>
-      <path d="M7.4 3.62a2.61 2.61 0 0 0-1.78.8 3.52 3.52 0 0 0-.88 2.3H4.5a.78.78 0 1 0 0 1.56h.24v.59a.78.78 0 0 0 0 .25 7.32 7.32 0 0 1-.31 2.62.78.78 0 0 0 .19 1.54 1.22 1.22 0 0 0 .89-.39 5 5 0 0 0 .79-3V9.57 8.28h.2a.78.78 0 0 0 0-1.56h-.2c.12-1.4.93-1.53 1.12-1.54a.78.78 0 0 0 0-1.56zM12.88 9.17L11.78 8l1-1.28a.78.78 0 1 0-1.21-1l-.86 1.08-.94-1a.78.78 0 1 0-1.15 1.02L9.72 8l-1 1.28a.78.78 0 1 0 1.21 1l.88-1.09.94 1a.78.78 0 0 0 1.15-1.05z" className="icon-inner"></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 16 16"
+    >
+      <path
+        d="M12 16H4a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4z"
+        className="icon-function"
+      ></path>
+      <path
+        d="M7.4 3.62a2.61 2.61 0 0 0-1.78.8 3.52 3.52 0 0 0-.88 2.3H4.5a.78.78 0 1 0 0 1.56h.24v.59a.78.78 0 0 0 0 .25 7.32 7.32 0 0 1-.31 2.62.78.78 0 0 0 .19 1.54 1.22 1.22 0 0 0 .89-.39 5 5 0 0 0 .79-3V9.57 8.28h.2a.78.78 0 0 0 0-1.56h-.2c.12-1.4.93-1.53 1.12-1.54a.78.78 0 0 0 0-1.56zM12.88 9.17L11.78 8l1-1.28a.78.78 0 1 0-1.21-1l-.86 1.08-.94-1a.78.78 0 1 0-1.15 1.02L9.72 8l-1 1.28a.78.78 0 1 0 1.21 1l.88-1.09.94 1a.78.78 0 0 0 1.15-1.05z"
+        className="icon-inner"
+      ></path>
     </svg>
-  )
+  );
 }
