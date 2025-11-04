648db17a7b7828007bdd6a9ed14e52148b3984f6
Jerel Miller <jerelmiller@gmail.com>
2025-11-04 00:53:02 -0700

    Extract ThemeDefinition

--- src/application/components/ObjectViewer/ObjectViewer.tsx
+++ src/application/components/ObjectViewer/ObjectViewer.tsx
@@ -1,9 +1,8 @@
 import { ValueNode } from "./ValueNode";
 import { colors } from "@apollo/brand";
-import { useThemeObject } from "@/application/hooks/useTheme";
-import type { ReactNode } from "react";
-import { useMemo } from "react";
 import { Provider } from "./context";
+import type { Theme } from "./ThemeDefinition";
+import { ThemeDefinition } from "./ThemeDefinition";
 
 export type Renderer<T = unknown> = {
   is: (value: unknown) => value is T;
@@ -13,36 +12,13 @@ export type Renderer<T = unknown> = {
 
 export interface CustomRenderProps<T = unknown> {
   value: T;
-  renderDefault: (value: unknown) => ReactNode;
+  DefaultRender: React.FC<{ className?: string; value: unknown }>;
 }
 
 interface Props {
   value: unknown;
   renderers: Record<string, Renderer<any>>;
 }
-
-type ThemeKey =
-  | "arrayIndex"
-  | "bracket"
-  | "constructorName"
-  | "ellipsis"
-  | "info"
-  | "punctuation"
-  | "quote"
-  | "objectKey"
-  | "typeBigint"
-  | "typeBoolean"
-  | "typeInt"
-  | "typeFloat"
-  | "typeFunction"
-  | "typeNaN"
-  | "typeNumber"
-  | "typeNull"
-  | "typeString"
-  | "typeSymbol"
-  | "typeUndefined";
-
-type Theme = Partial<Record<ThemeKey, { base: string; dark: string }>>;
 
 const { code, text } = colors.tokens;
 
@@ -62,22 +38,11 @@ const theme: Theme = {
 };
 
 export function ObjectViewer({ renderers, value }: Props) {
-  const themeObject = useThemeObject(theme);
-
-  const themeValues = useMemo(() => {
-    return Object.fromEntries(
-      Object.entries(themeObject).map(([key, value]) => [
-        `--ov-${key}-color`,
-        value,
-      ])
-    );
-  }, [themeObject]);
-
   return (
-    <div style={themeValues} className="font-code">
+    <ThemeDefinition theme={theme} className="font-code">
       <Provider renderers={renderers}>
         <ValueNode depth={0} value={value} />
       </Provider>
-    </div>
+    </ThemeDefinition>
   );
 }
