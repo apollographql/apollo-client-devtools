cd09d8bced5449df04353ea031754929f9fd8729
Jerel Miller <jerelmiller@gmail.com>
2025-11-05 00:06:47 -0700

    Add brace components

---
+++ src/application/components/ObjectViewer/CloseBracket.tsx
@@ -0,0 +1,7 @@
+export function CloseBracket() {
+  return (
+    <span className="inline-block align-middle text-[var(--ov-bracket-color,var(--ov-punctuation-color))]">
+      {"]"}
+    </span>
+  );
+}
