ff764be3279a841de90202202932b30747a077c2
Jerel Miller <jerelmiller@gmail.com>
2025-11-05 00:33:08 -0700

    Copy IterableItem to ObjectPair

--- src/application/components/ObjectViewer/ObjectPair.tsx
+++ src/application/components/ObjectViewer/ObjectPair.tsx
@@ -1,7 +1,12 @@
-import type { ComponentPropsWithoutRef } from "react";
+import IconChevronRight from "@apollo/icons/default/IconChevronRight.svg";
+import { useState, type ComponentPropsWithoutRef } from "react";
 import { customRenderable } from "./CustomRenderable";
-import { IterableItem } from "./IterableItem";
 import type { Path } from "./types";
+import { clsx } from "clsx";
+import { ObjectKey } from "./ObjectKey";
+import { ValueNode } from "./ValueNode";
+import { CollapsedObject } from "./CollapsedObject";
+import { CollectionLength } from "./CollectionLength";
 
 interface ObjectPairProps extends ComponentPropsWithoutRef<"div"> {
   context: Record<string, any> | undefined;
@@ -24,16 +29,57 @@ export const ObjectPair = customRenderable(
       (typeof value === "object" && value !== null),
     ...rest
   }: ObjectPairProps) => {
+    const [expanded, setExpanded] = useState(expandable ? depth === 0 : true);
+
     return (
-      <IterableItem
-        {...rest}
-        expandable={expandable}
-        context={context}
-        depth={depth}
-        itemKey={objectKey}
-        value={value}
-        path={path}
-      />
+      <div {...rest}>
+        <span
+          className={clsx("inline-block align-middle relative", {
+            "cursor-pointer": expandable,
+          })}
+          onClick={
+            expandable ? () => setExpanded((expanded) => !expanded) : undefined
+          }
+        >
+          <span className="inline-flex items-center">
+            {expandable && (
+              <IconChevronRight
+                className={clsx(
+                  "block size-4 transition-transform ease-out absolute -left-5 top-1/2 -translate-y-1/2",
+                  {
+                    "rotate-90": expanded,
+                  }
+                )}
+              />
+            )}
+            <ObjectKey context={context} value={objectKey} />
+            <span className="text-[var(--ov-punctuation-color)]">:</span>
+          </span>
+        </span>{" "}
+        {expanded ? (
+          <>
+            <ValueNode
+              className="align-middle"
+              context={context}
+              depth={depth}
+              value={value}
+              path={path}
+            />
+            <span className="text-[var(--ov-punctuation-color)]">,</span>
+          </>
+        ) : (
+          <span className="inline-block align-middle">
+            <CollapsedObject
+              context={context}
+              onClick={() => setExpanded(true)}
+            />
+            <span className="text-[var(--ov-punctuation-color)]">,</span>{" "}
+            {!expanded && typeof value === "object" && value !== null && (
+              <CollectionLength className="italic" value={value} />
+            )}
+          </span>
+        )}
+      </div>
     );
   },
   (parentProps, props: Partial<Omit<ObjectPairProps, "depth" | "path">>) => ({
