f70b8669b4caf4668662310b32d2b84761951831
Jerel Miller <jerelmiller@gmail.com>
2025-11-04 17:12:12 -0700

    Extract shared props

--- src/application/components/ObjectViewer/ValueNode.tsx
+++ src/application/components/ObjectViewer/ValueNode.tsx
@@ -37,90 +37,39 @@ export const ValueNode = ({ context, className, depth, value }: Props) => {
     [context, depth]
   );
 
+  const sharedProps = {
+    depth,
+    className,
+    context,
+  };
+
   switch (type) {
     case "array":
-      return (
-        <ArrayNode
-          depth={depth}
-          className={className}
-          value={value as unknown[]}
-        />
-      );
+      return <ArrayNode {...sharedProps} value={value as unknown[]} />;
     case "bigint":
-      return (
-        <BigintNode
-          depth={depth}
-          className={className}
-          value={value as bigint}
-        />
-      );
+      return <BigintNode {...sharedProps} value={value as bigint} />;
     case "boolean":
-      return (
-        <BooleanNode
-          context={context}
-          depth={depth}
-          className={className}
-          value={value as boolean}
-        />
-      );
+      return <BooleanNode {...sharedProps} value={value as boolean} />;
     case "function":
-      return (
-        <FunctionNode
-          depth={depth}
-          className={className}
-          value={value as Function}
-        />
-      );
+      return <FunctionNode {...sharedProps} value={value as Function} />;
     case "number":
-      return (
-        <NumberNode
-          depth={depth}
-          className={className}
-          value={value as number}
-        />
-      );
+      return <NumberNode {...sharedProps} value={value as number} />;
     case "null":
-      return <NullNode depth={depth} className={className} />;
+      return <NullNode {...sharedProps} />;
     case "string":
-      return (
-        <StringNode
-          context={context}
-          depth={depth}
-          className={className}
-          value={value as string}
-        />
-      );
+      return <StringNode {...sharedProps} value={value as string} />;
     case "symbol":
-      return (
-        <SymbolNode
-          depth={depth}
-          className={className}
-          value={value as symbol}
-        />
-      );
+      return <SymbolNode {...sharedProps} value={value as symbol} />;
     case "undefined":
-      return <UndefinedNode depth={depth} className={className} />;
+      return <UndefinedNode {...sharedProps} />;
     case "object": {
-      return (
-        <ObjectNode
-          context={context}
-          className={className}
-          depth={depth}
-          value={value as object}
-        />
-      );
+      return <ObjectNode {...sharedProps} value={value as object} />;
     }
     default: {
       const Render = ctx.renderers[type];
 
       return Render ? (
-        <Render
-          depth={depth}
-          className={className}
-          context={context}
-          DefaultRender={DefaultRender}
-          value={value}
-        />
+        <Render {...sharedProps} DefaultRender={DefaultRender} value={value} />
       ) : (
         <DefaultRender className={className} value={value} />
       );
