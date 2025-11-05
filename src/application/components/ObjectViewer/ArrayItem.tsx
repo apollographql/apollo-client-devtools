804e0a86ac16ba96289bcb73149c578defe04761
Jerel Miller <jerelmiller@gmail.com>
2025-11-04 21:04:19 -0700

    Better handle omitted props in default render

--- src/application/components/ObjectViewer/ArrayItem.tsx
+++ src/application/components/ObjectViewer/ArrayItem.tsx
@@ -1,6 +1,15 @@
 import { customRenderable } from "./CustomRenderable";
 import { IterableItem } from "./IterableItem";
 
+interface ArrayItemProps {
+  className?: string;
+  context: Record<string, any> | undefined;
+  expandable?: boolean;
+  depth: number;
+  index: number;
+  value: unknown;
+}
+
 export const ArrayItem = customRenderable(
   "arrayItem",
   ({
@@ -11,14 +20,7 @@ export const ArrayItem = customRenderable(
     value,
     expandable = Array.isArray(value) ||
       (typeof value === "object" && value !== null),
-  }: {
-    className?: string;
-    context: Record<string, any> | undefined;
-    expandable?: boolean;
-    depth: number;
-    index: number;
-    value: unknown;
-  }) => {
+  }: ArrayItemProps) => {
     return (
       <IterableItem
         expandable={expandable}
@@ -29,5 +31,10 @@ export const ArrayItem = customRenderable(
         value={value}
       />
     );
-  }
+  },
+  (parentProps, props: Partial<Omit<ArrayItemProps, "depth">>) => ({
+    ...parentProps,
+    ...props,
+    depth: parentProps.depth,
+  })
 );
