85204ac65ff8def8a08a4992d9e03eecffeceaf7
Jerel Miller <jerelmiller@gmail.com>
2025-11-06 15:17:07 -0700

    Don't collapse query options

--- src/application/components/Queries/Queries.tsx
+++ src/application/components/Queries/Queries.tsx
@@ -262,6 +262,7 @@ export const Queries = ({ clientId, explorerIFrame }: QueriesProps) => {
               <ObjectViewer
                 value={selectedQuery?.options}
                 displayObjectSize={false}
+                collapsed={false}
               />
             </QueryLayout.TabContent>
           </QueryLayout.Tabs>
