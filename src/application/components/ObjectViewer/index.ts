64e1e69dd8c486d68747abeeaa5e9997874f18d4
Jerel Miller <jerelmiller@gmail.com>
2025-11-06 10:17:58 -0700

    Export RenderableType

--- src/application/components/ObjectViewer/index.ts
+++ src/application/components/ObjectViewer/index.ts
@@ -1,5 +1,5 @@
 export type { ColorValue } from "./useObjectViewerTheme";
 export type { Path } from "./types";
-export type { BuiltinRendererProps } from "./ObjectViewer";
+export type { BuiltinRendererProps, RenderableType } from "./ObjectViewer";
 export { ObjectViewer } from "./ObjectViewer";
 export { useGetObjectViewerThemeOverride } from "./useObjectViewerTheme";
