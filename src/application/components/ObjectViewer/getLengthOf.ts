c5d4259820a0e1ebc6ab1819a6d7c33db058ed63
Jerel Miller <jerelmiller@gmail.com>
2025-11-05 18:14:20 -0700

    Don't set empty object/array as expandable

--- src/application/components/ObjectViewer/getLengthOf.ts
+++ src/application/components/ObjectViewer/getLengthOf.ts
@@ -1,3 +1,7 @@
+/**
+ * Returns the length of a value such as an array or object. Return of -1
+ * means the value is not an array or object.
+ */
 export function getLengthOf(value: unknown) {
   if (Array.isArray(value)) {
     return value.length;
