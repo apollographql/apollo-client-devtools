70d810ea398175e71555747a2a7f74e1c276d18b
Jerel Miller <jerelmiller@gmail.com>
2025-11-07 13:19:04 -0700

    Switch to master/detail view

--- src/application/components/Cache/common/CacheWritesPanel.tsx
+++ src/application/components/Cache/common/CacheWritesPanel.tsx
@@ -1,18 +1,18 @@
 import IconUnavailable from "@apollo/icons/default/IconUnavailable.svg";
+import IconArrowLeft from "@apollo/icons/default/IconArrowLeft.svg";
 import { fragmentRegistry } from "@/application/fragmentRegistry";
 import type { Client } from "@/application/types/gql";
 import { type CacheWritesPanelFragment } from "@/application/types/gql";
 import type { TypedDocumentNode } from "@apollo/client";
 import { gql } from "@apollo/client";
 import { useFragment } from "@apollo/client/react";
-import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { Panel } from "react-resizable-panels";
 import { CodeBlock } from "../../CodeBlock";
-import { memo, useState } from "react";
+import { useState } from "react";
 import { getOperationName } from "@apollo/client/utilities/internal";
 import { List } from "../../List";
 import { ListItem } from "../../ListItem";
 import { format } from "date-fns";
-import type { Diff } from "@/application/utilities/diff";
 import { ObjectDiff } from "../../ObjectDiff";
 import { ObjectViewer } from "../../ObjectViewer";
 import { VariablesObject } from "../../VariablesObject";
@@ -45,13 +45,12 @@ interface Props {
 }
 
 export function CacheWritesPanel({ client, cacheWrites }: Props) {
-  const apolloClient = useApolloClient();
   const { data, complete } = useFragment({
     fragment: CACHE_WRITES_PANEL_FRAGMENT,
     from: cacheWrites,
   });
 
-  const [selectedId, setSelectedId] = useState<string>();
+  const [selectedId, setSelectedId] = useState<string | null>(null);
 
   if (!complete) {
     return null;
@@ -68,102 +67,121 @@ export function CacheWritesPanel({ client, cacheWrites }: Props) {
       minSize={10}
       defaultSize={25}
     >
-      <PanelGroup direction="horizontal" className="flex grow">
-        <Panel id="cacheWriteList" className="grow !overflow-auto" minSize={25}>
-          <section className="flex items-center justify-between border-b border-b-primary dark:border-b-primary-dark py-2 px-4">
-            <h2 className="grow font-medium text-lg text-heading dark:text-heading-dark">
-              Cache writes ({cacheWrites.length})
-            </h2>
-            <div>
-              <Tooltip content="Clear">
-                <Button
-                  aria-label="Clear"
-                  size="sm"
-                  variant="hidden"
-                  onClick={() => {
-                    if (client) {
-                      apolloClient.cache.modify<Client>({
-                        id: apolloClient.cache.identify(client),
-                        fields: {
-                          cacheWrites: () => [],
-                        },
-                      });
-                    }
-                  }}
-                  icon={<IconUnavailable />}
-                />
-              </Tooltip>
-            </div>
-          </section>
-          <List className="p-4">
-            {[...data].reverse().map((cacheWrite) => (
-              <ListItem
-                key={cacheWrite.id}
-                onClick={() => setSelectedId(cacheWrite.id)}
-                selected={cacheWrite.id === selectedId}
-              >
-                <div className="flex flex-col gap-1">
-                  <span className="font-code">
-                    {getOperationName(cacheWrite.document.ast)}
-                  </span>
-                  <span className="text-xs">
-                    {format(new Date(cacheWrite.timestamp), "MMM do, yyyy pp")}
-                  </span>
-                </div>
-              </ListItem>
-            ))}
-          </List>
-        </Panel>
-        {selectedCacheWrite && (
-          <>
-            <PanelResizeHandle className="border-l border-l-primary dark:border-l-primary-dark" />
-            <Panel
-              id="cacheWriteDetails"
-              className="flex flex-col gap-4 grow p-4 !overflow-auto"
-            >
-              <div className="flex flex-col gap-2">
-                <CodeBlock
-                  language="graphql"
-                  code={selectedCacheWrite.document.string}
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <h2 className="text-md text-heading dark:text-heading-dark">
-                  Data
-                </h2>
-                <ObjectViewer value={selectedCacheWrite.data} />
-              </div>
-              <div className="flex flex-col gap-2">
-                <h2 className="text-md text-heading dark:text-heading-dark">
-                  Variables
-                </h2>
-                <VariablesObject
-                  variables={selectedCacheWrite.variables ?? undefined}
-                />
-              </div>
-              <DiffView
-                key={selectedCacheWrite.id}
-                diff={selectedCacheWrite.cacheDiff}
-              />
-            </Panel>
-          </>
-        )}
-      </PanelGroup>
+      {selectedCacheWrite ? (
+        <CacheWriteView
+          cacheWrite={selectedCacheWrite}
+          onNavigateBack={() => setSelectedId(null)}
+        />
+      ) : (
+        <ListView client={client} cacheWrites={data} onSelect={setSelectedId} />
+      )}
     </Panel>
   );
 }
 
-const DiffView = memo(({ diff }: { diff: Diff | null }) => {
+function ListView({
+  client,
+  cacheWrites,
+  onSelect,
+}: {
+  client:
+    | { __typename: "ClientV3" | "ClientV4"; id: string }
+    | null
+    | undefined;
+  cacheWrites: CacheWritesPanelFragment[];
+  onSelect: (cacheWriteId: string) => void;
+}) {
+  const apolloClient = useApolloClient();
+
   return (
-    <div className="flex flex-col gap-2">
-      <h2 className="text-md text-heading dark:text-heading-dark">Diff</h2>
-      {diff === null ? (
-        <span className="text-secondary dark:text-secondary-dark italic">
-          Unchanged
-        </span>
-      ) : (
-        <ObjectDiff diff={diff} />
-      )}
+    <div className="grow !overflow-auto">
+      <section className="flex items-center justify-between border-b border-b-primary dark:border-b-primary-dark py-2 px-4">
+        <h2 className="grow font-medium text-lg text-heading dark:text-heading-dark">
+          Cache writes ({cacheWrites.length})
+        </h2>
+        <Tooltip content="Clear">
+          <Button
+            aria-label="Clear"
+            size="sm"
+            variant="hidden"
+            onClick={() => {
+              if (client) {
+                apolloClient.cache.modify<Client>({
+                  id: apolloClient.cache.identify(client),
+                  fields: {
+                    cacheWrites: () => [],
+                  },
+                });
+              }
+            }}
+            icon={<IconUnavailable />}
+          />
+        </Tooltip>
+      </section>
+      <List className="p-4">
+        {[...cacheWrites].reverse().map((cacheWrite) => (
+          <ListItem key={cacheWrite.id} onClick={() => onSelect(cacheWrite.id)}>
+            <div className="flex flex-col gap-1">
+              <span className="font-code">
+                {getOperationName(cacheWrite.document.ast)}
+              </span>
+              <span className="text-xs">
+                {format(new Date(cacheWrite.timestamp), "MMM do, yyyy pp")}
+              </span>
+            </div>
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
-});
+}
+
+function CacheWriteView({
+  cacheWrite,
+  onNavigateBack,
+}: {
+  cacheWrite: CacheWritesPanelFragment;
+  onNavigateBack: () => void;
+}) {
+  return (
+    <div className="grow !overflow-auto">
+      <section className="flex items-center gap-2 border-b border-b-primary dark:border-b-primary-dark py-2 px-4">
+        <Tooltip content="Back">
+          <Button
+            aria-label="Back"
+            variant="hidden"
+            size="sm"
+            icon={<IconArrowLeft />}
+            onClick={onNavigateBack}
+          />
+        </Tooltip>
+        <h2 className="grow font-medium text-lg text-heading dark:text-heading-dark font-code">
+          {getOperationName(cacheWrite.document.ast, "(anonymous)")}
+        </h2>
+      </section>
+      <div className="flex flex-col gap-4 p-4">
+        <CodeBlock language="graphql" code={cacheWrite.document.string} />
+        <div className="flex flex-col gap-2">
+          <h3 className="text-md text-heading dark:text-heading-dark">Data</h3>
+          <ObjectViewer value={cacheWrite.data} />
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="text-md text-heading dark:text-heading-dark">
+            Variables
+          </h3>
+          <VariablesObject variables={cacheWrite.variables ?? undefined} />
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="text-md text-heading dark:text-heading-dark">Diff</h3>
+          {cacheWrite.cacheDiff === null ? (
+            <span className="text-secondary dark:text-secondary-dark italic">
+              Unchanged
+            </span>
+          ) : (
+            <ObjectDiff diff={cacheWrite.cacheDiff} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
