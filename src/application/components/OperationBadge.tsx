6fb027bea2e8962260c42785d0f332f48046d065
Jerel Miller <jerelmiller@gmail.com>
2025-11-10 21:12:22 -0700

    Allow any document node for OperationBadge. Minor change to style

--- src/application/components/OperationBadge.tsx
+++ src/application/components/OperationBadge.tsx
@@ -1,17 +1,26 @@
 import type { DocumentNode } from "@apollo/client";
-import { getOperationDefinition } from "@apollo/client/utilities/internal";
+import {
+  getMainDefinition,
+  getOperationDefinition,
+} from "@apollo/client/utilities/internal";
 import { Badge } from "./Badge";
+import { Kind } from "graphql";
 
 interface Props {
   document: DocumentNode;
 }
 
 export function OperationBadge({ document }: Props) {
-  const { operation } = getOperationDefinition(document)!;
+  const definition = getMainDefinition(document);
+  const kind =
+    definition.kind === Kind.OPERATION_DEFINITION ? definition.operation : "F";
 
   return (
-    <Badge variant="info" className="font-code">
-      {operation.at(0)?.toUpperCase()}
+    <Badge
+      variant="info"
+      className="font-code shrink-0 size-5 inline-flex items-center justify-center"
+    >
+      {kind.at(0)?.toUpperCase()}
     </Badge>
   );
 }
