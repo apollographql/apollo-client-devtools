2325de73caaa1896a701f3ca2c21822add7daae6
Jerel Miller <jerelmiller@gmail.com>
2025-11-21 12:35:45 -0700

    Reply with post message error in message bridge

--- src/extension/messages.ts
+++ src/extension/messages.ts
@@ -1,16 +1,26 @@
+import type { SerializedErrorLike } from "@/application/errors";
 import type { ApolloClientDevtoolsActorMessage } from "./actor";
 import type { RPCRequestMessage, RPCResponseMessage } from "./rpc";
+
+export interface PostMessageError {
+  source: "apollo-client-devtools";
+  type: MessageType.PostMessageError;
+  id: string;
+  error: SerializedErrorLike;
+}
 
 export const enum MessageType {
   RPCRequest = "rpcRequest",
   RPCResponse = "rpcResponse",
   Actor = "actor",
+  PostMessageError = "postMessageError",
 }
 
 export type ApolloClientDevtoolsMessage =
   | ApolloClientDevtoolsActorMessage
   | RPCRequestMessage
-  | RPCResponseMessage;
+  | RPCResponseMessage
+  | PostMessageError;
 
 export function isDevtoolsMessage(
   message: unknown
@@ -20,5 +30,13 @@ export function isDevtoolsMessage(
     message !== null &&
     "source" in message &&
     message.source === "apollo-client-devtools"
+  );
+}
+
+export function isPostMessageError(
+  message: unknown
+): message is PostMessageError {
+  return (
+    isDevtoolsMessage(message) && message.type === MessageType.PostMessageError
   );
 }
