09f7c59e49ca8d60c3a91fd637287f74790d79f7
Jerel Miller <jerelmiller@gmail.com>
2025-12-10 16:51:18 -0700

    Add custom equality testers for diff instances

---
+++ jest/equalityTesters/diff.ts
@@ -0,0 +1,47 @@
+import type { Tester } from "@jest/expect-utils";
+import { Added, Changed, Deleted } from "../../src/application/utilities/diff";
+
+export const areAddedInstancesEqual: Tester = function (a, b, customTesters) {
+  const isAAdded = a instanceof Added;
+  const isBAdded = b instanceof Added;
+
+  if (isAAdded && isBAdded) {
+    return this.equals(a.value, b.value, customTesters);
+  } else if (isAAdded === isBAdded) {
+    return undefined;
+  } else {
+    return false;
+  }
+};
+
+export const areChangedInstancesEqual: Tester = function (a, b, customTesters) {
+  const isAChanged = a instanceof Changed;
+  const isBChanged = b instanceof Changed;
+
+  if (isAChanged && isBChanged) {
+    return (
+      this.equals(a.oldValue, b.oldValue, customTesters) &&
+      this.equals(a.newValue, b.newValue, customTesters)
+    );
+  } else if (isBChanged === isBChanged) {
+    return undefined;
+  } else {
+    return false;
+  }
+};
+
+export const areDeletedInstancesEqual: Tester = function (a, b, customTesters) {
+  const isADeleted = a instanceof Deleted;
+  const isBDeleted = b instanceof Deleted;
+
+  if (isADeleted && isBDeleted) {
+    return (
+      this.equals(a.value, b.value, customTesters) &&
+      this.equals(a.value, b.value, customTesters)
+    );
+  } else if (isBDeleted === isBDeleted) {
+    return undefined;
+  } else {
+    return false;
+  }
+};
