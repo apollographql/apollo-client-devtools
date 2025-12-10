c1395c3894a6a2b73b82078a8eed74aca42a25c4
Jerel Miller <jerelmiller@gmail.com>
2025-12-09 18:04:09 -0700

    Add cache write limit to settings

--- src/application/components/Layouts/SettingsModal.tsx
+++ src/application/components/Layouts/SettingsModal.tsx
@@ -1,6 +1,7 @@
 import { Button } from "../Button";
 import { ExternalLink } from "../ExternalLink";
 import { Modal } from "../Modal";
+import { TextField } from "../TextField";
 
 declare const VERSION: string;
 
@@ -12,18 +13,27 @@ export function SettingsModal({
   onOpen: (open: boolean) => void;
 }) {
   return (
-    <Modal open={open} onClose={onOpen} size="sm">
+    <Modal open={open} onClose={onOpen} size="md">
       <Modal.Header>
         <Modal.Title>Settings</Modal.Title>
+        <Modal.Description>
+          Devtools version:{" "}
+          <ExternalLink
+            className="font-code"
+            href={`https://github.com/apollographql/apollo-client-devtools/releases/tag/apollo-client-devtools@${VERSION}`}
+          >
+            {VERSION}
+          </ExternalLink>
+        </Modal.Description>
       </Modal.Header>
       <Modal.Body>
-        Devtools version:{" "}
-        <ExternalLink
-          className="font-code"
-          href={`https://github.com/apollographql/apollo-client-devtools/releases/tag/apollo-client-devtools@${VERSION}`}
-        >
-          {VERSION}
-        </ExternalLink>
+        <TextField
+          label="Cache write limit"
+          size="sm"
+          placeholder="Enter a max limit"
+          type="number"
+          defaultValue={500}
+        />
       </Modal.Body>
       <Modal.Footer>
         <Button variant="primary" size="md" onClick={() => onOpen(false)}>
