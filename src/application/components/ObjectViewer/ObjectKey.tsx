325a5a84a3270585b1f7c635fb50ea8105548376
Jerel Miller <jerelmiller@gmail.com>
2025-12-11 19:02:35 -0700

    Add ability to soft wrap characters in object key

--- src/application/components/ObjectViewer/ObjectKey.tsx
+++ src/application/components/ObjectViewer/ObjectKey.tsx
@@ -1,19 +1,27 @@
 import { clsx } from "clsx";
 import { customRenderable } from "./CustomRenderable";
+import { Join } from "../Join";
 
 interface ObjectKeyProps {
   context?: Record<string, any>;
   className?: string;
   value: string;
+  softWrapCharacters?: string[];
 }
 
 export const ObjectKey = customRenderable(
   "objectKey",
-  ({ className, value }: ObjectKeyProps) => (
-    <span className={clsx("text-[var(--ov-objectKey-color)]", className)}>
-      {value}
-    </span>
-  ),
+  ({ className, value, softWrapCharacters }: ObjectKeyProps) => {
+    const regex = softWrapCharacters
+      ? new RegExp(`(${softWrapCharacters.join("|")})`)
+      : null;
+
+    return (
+      <span className={clsx("text-[var(--ov-objectKey-color)]", className)}>
+        {regex ? <Join delimeter={<wbr />}>{value.split(regex)}</Join> : value}
+      </span>
+    );
+  },
   (parentProps, props: Omit<ObjectKeyProps, "value">) => ({
     ...parentProps,
     ...props,
